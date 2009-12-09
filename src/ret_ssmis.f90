3e31d7a0b5d68c59ea736a243fe608848b6ac315
Michael.Lueken <Michael.Lueken@noaa.gov>
2009-12-09 14:01:44 +0000

    Trunk: merged Banghua's changes r5970:5984 to the trunk.  This change rewrites the clw retrieval code for ssmis sensor (ret_ssmis.f90).  Correcting for incorrect remapping of ssmis to ssmi.

--- src/ret_ssmis.f90
+++ src/ret_ssmis.f90
@@ -13,12 +13,13 @@ subroutine ret_ssmis(tb,nchanl, &
 !
 !     These algorithm are based on
 !
-!        "Effects of AMSU cross-scan Symmetry of brightness temperatures
-!         on  retrieval of atmospheric and surface parameters" 
+!     Weng, F., R. R. Ferraro, and N. C. Grody,2000: "Effects of AMSU cross-scan Symmetry of 
+!          brightness temperatures on  retrieval of atmospheric and surface parameters", 
+!          Ed. P. Pampaloni and S. Paloscia, VSP, Netherlands, 255-262, 2000 
 !
-!     by Weng, F., R. R. Ferraro, and N. C. Grody, published in 
-!     "Microwave Radiometry & Remote Sensing of the Earth Surface and Atmosphere"
-!     Ed. P. Pampaloni and S. Paloscia, VSP, Netherlands, 255-262, 2000. 
+!     Yan B. and F. Weng, 'Intercalibration between Special Sensor Microwave Imager and Sounder (SSMIS)
+!         and Special Sensor Microwave Imager (SSM/I)', TGARS Special Issue on the DMSP SSMIS, 46, 984-995.
+!
 !
 !     tpw:: total precipitable water 
 !     clw :: column cloud water ove ocean  [kg/m2]
@@ -36,11 +37,32 @@ subroutine ret_ssmis(tb,nchanl, &
 !                            and bias correction of retrieved clw.
 !     2008-04-16  safford  - rm unused uses and vars
 !
+!     2009-12-07  b.yan    - rewrite the ssmis TA level CLW agorithm and 
+!                            remove the sea ice identification algorithm since this code frequently 
+!                                   mis-identify ocean-pixels as sea ice-pixels
+!
+!     Reasons: in the previous version,there are the following bugs
+!              (a) the ssmis data used in the gsi is tb level insetad of ta level
+!              (b) the ssmis remapping coefficients are derived at ta level instead of tb level
+!              (c) the polarization of the ssmis data is mis-used which will result in a totally wrong
+!                  cloud liquid water path calculation
+!              (d) the comments for the coefficients are not correct
+!     In the new version of the code:
+!              (a) TB2TA is needed to use the ssmis remapping coefficients derived at the ta level
+!              (b) the remapping coefficients are updated , which are derived using the ssmis/ssmi SCO
+!                  observations
+!              (c) add the correct comments
+!              (d) remove sea ice index and subroutine
+!              (e) remove ssmis_las check if it is not necessary
 !   input argument list:
 !     tb      - Observed brightness temperature [K]
 !     nchanl  - number of channels per obs
-!     ssmis_las    - logical true if ssmis_las (channel 1,2,3,4,5,6,7,24) is processed
-!
+!     
+!   Internal variable list:
+!     tbx    - brightness temperatures at seven window channels
+!     tby    - brightness temperatures at seven window channels which are consistent to f15 ssmi tb results
+!     tax    - ssmis antenna temperatures at seven window channels
+!     tay    - remapped ssmis antenna temperatures at seven window channels
 !   output argument list:
 !     clw     - column cloud water vapor over ocean  [kg/m2]
 !     ierr    - error flag
@@ -73,74 +95,82 @@ subroutine ret_ssmis(tb,nchanl, &
   integer(i_kind) :: iclwflg
   real(r_kind),parameter:: r285=285.0_r_kind
   real(r_kind),parameter:: r290=290.0_r_kind
-  real(r_kind),dimension(7):: ap,bp,cp,dp,cp0,dp0,tby,tbx
+  real(r_kind),dimension(7):: ap,bp,cp,dp,cp0,dp0,tbx,tby,tax,tay
   real(r_kind):: alg1,alg2,alg3
-  real(r_kind):: seaice
 
   ierr = izero
   clw = rmis
 
-  if(ssmis_las) then
-
-!    ap and bp for the cloud liquid water mass absorption coefficients calculation 
-     ap = (/7.80472_r_kind, 7.44254_r_kind, 6.76383_r_kind, 7.34409_r_kind, &
-          8.55426_r_kind, 6.57813_r_kind,6.45397_r_kind/)
-     bp = (/0.967519_r_kind, 0.969424_r_kind, 0.959808_r_kind, 0.958955_r_kind, &
-          0.954316_r_kind, 0.980339_r_kind, 0.978795_r_kind/)
+! Coefficients to remap SSMIS ssmi-like channels to SSMI TB
+! Simultaneous Conical Overpass (SCO) data set-derived coefficients which are more accurate
+!
+     ap = (/0.00424_r_kind, -2.03627_r_kind, -2.52875_r_kind, 0.80170_r_kind, &
+          -3.86053_r_kind, -7.43913_r_kind,1.53650_r_kind/)
+     bp = (/1.00027_r_kind, 1.00623_r_kind, 0.99642_r_kind, 0.99139_r_kind, &
+          1.00550_r_kind, 1.03121_r_kind, 0.99317_r_kind/)
      
-!    cp0 and dp0 for Oxygen optical thickness parameterization 
+!    cp0 and dp0 for TA2TB
      cp0 = (/.969_r_kind, .969_r_kind, .974_r_kind, .986_r_kind,  &
-          .986_r_kind, .988_r_kind, .988_r_kind/)
-     dp0 = (/.00473_r_kind, .00415_r_kind, .0107_r_kind, .0217_r_kind, &
-          .02612_r_kind, .01383_r_kind, .01947_r_kind/)
-
+             .986_r_kind, .988_r_kind, .988_r_kind/)
+     dp0 = (/.00415_r_kind,.00473_r_kind,.0107_r_kind,.02612_r_kind,  &
+              .0217_r_kind, .01383_r_kind, .01947_r_kind/)
 
      alg1 = rmis; alg2 = rmis; alg3 = rmis 
      tbx(:) = rmis;  tby(:) = rmis
-     
-!    Mimic ssmis imager channels to corresponding ssmis channels
+ 
+!   save brightness temperatures at seven window channels
+    tbx(1:7) = tb(12:18)
 
-!    The coefficients (tbx) are derived from tb bias  
-!    original env combination does not contan 91GHz, so nchanl=5 
+!   get ta2tb coefficients
      do i=1,7
-        tbx(i) =  ap(i) + bp(i)*tb(i+11)
-     end do
-
-!    Coefficients for tbx to tby conversion 
-     do i=1,7
-        cp(i) = one/( cp0(i)*(one-dp0(i)) ) 
+        cp(i) = one/( cp0(i)*(one-dp0(i)) )
         dp(i) = cp(i) * dp0(i)
      end do
-     
-     tby(1)  =  cp(1)*tbx(1) - dp(1)*tbx(2);  
-     tby(2)  =  cp(2)*tbx(2) - dp(2)*tbx(1);  
-     tby(3)  =  cp(3)*tbx(3) - dp(3)*(.653_r_kind*tbx(2) + 96.6_r_kind);
-     tby(4)  =  cp(4)*tbx(4) - dp(4)*tbx(5);  
-     tby(5)  =  cp(5)*tbx(5) - dp(5)*tbx(4);  
-     tby(6)  =  cp(6)*tbx(6) - dp(6)*tbx(7);  
-     tby(7)  =  cp(7)*tbx(7) - dp(7)*tbx(6);  
 
-!    Ice check
-     call ice_check(tbx,seaice,ierr)
-     if(ierr==3) return
+!   get ta from tb
+    tax(1) = (tbx(1)*cp(2) + tbx(2)*dp(1))/(cp(1)*cp(2) - dp(1)*dp(2))
+    tax(2) = (tbx(1)*dp(2) + tbx(2)*cp(1))/(cp(1)*cp(2) - dp(1)*dp(2))
+    tax(3) = 1.0/cp(3)*(tbx(3) + dp(3)*(.653_r_kind*tax(2)+ 96.6_r_kind))
+    tax(4) = (tbx(4)*cp(5) + tbx(5)*dp(4))/(cp(4)*cp(5) - dp(4)*dp(5))
+    tax(5) = (tbx(4)*dp(5) + tbx(5)*cp(4))/(cp(4)*cp(5) - dp(4)*dp(5))
+    tax(6) = (tbx(6)*cp(7) + tbx(7)*dp(6))/(cp(6)*cp(7) - dp(6)*dp(7))
+    tax(7) = (tbx(6)*dp(7) + tbx(7)*cp(6))/(cp(6)*cp(7) - dp(6)*dp(7))
+
+!    TAREMAPPING: Mimic ssmis imager channels to corresponding ssmis channels at ta level
+!    (So, tay is antenna temperature consistent to f15 ssmi ta)
+     do i=1,7
+        tay(i) =  ap(i) + bp(i)*tax(i)
+     end do
+
+!   Get ssmis tb consisitent to f15 ssmi tb
+!    TA2TB(F15): Coefficients for tay to tby conversion
+
+     tby(1)  =  cp(1)*tay(1) - dp(1)*tay(2);
+     tby(2)  =  cp(2)*tay(2) - dp(2)*tay(1);
+     tby(3)  =  cp(3)*tay(3) - dp(3)*(.653_r_kind*tay(2) + 96.6_r_kind);
+     tby(4)  =  cp(4)*tay(4) - dp(4)*tay(5);
+     tby(5)  =  cp(5)*tay(5) - dp(5)*tay(4);
+     tby(6)  =  cp(6)*tay(6) - dp(6)*tay(7);
+     tby(7)  =  cp(7)*tay(7) - dp(7)*tay(6);
 
 !    Calculate parameter for clw
      tpwc = 232.89_r_kind - 0.1486_r_kind*tby(1) - 0.3695_r_kind*tby(4)  & 
           - ( 1.8291_r_kind - 0.006193_r_kind*tby(3) )*tby(3)
      
-     if( tbx(1)<r285 .and. tbx(3)<r285 ) then
+!   TA level CLW algorithm
+    if( tay(2)<r285 .and. tay(3)<r285 ) then
         alg1 = -3.20_r_kind*(  &
-             log( r290-tbx(1) ) - 2.80_r_kind - 0.42_r_kind*log( r290-tbx(3) )  )
+             log( r290-tay(2) ) - 2.80_r_kind - 0.42_r_kind*log( r290-tay(3) )  )
      end if
-     
-     if( tbx(4)<r285 .and. tbx(3)<r285 ) then
+
+     if( tay(5)<r285 .and. tay(3)<r285 ) then
         alg2 = -1.66_r_kind*(  &
-             log( r290-tbx(4) ) - 2.90_r_kind - 0.340_r_kind*log( r290-tbx(3) )  );
+             log( r290-tay(5) ) - 2.90_r_kind - 0.349_r_kind*log( r290-tay(3) )  );
      end if
-     
-     if( tbx(7)<r285 .and. tbx(3)<r285 ) then
+
+     if( tay(7)<r285 .and. tay(3)<r285 ) then
         alg3 = -0.44_r_kind*( &
-             log( r290-tbx(7) ) + 1.60_r_kind - 1.354_r_kind*log( r290-tbx(3) )  )
+             log( r290-tay(7) ) + 1.60_r_kind - 1.354_r_kind*log( r290-tay(3) )  )
      end if
 
 !    Determine clw
@@ -160,61 +190,11 @@ subroutine ret_ssmis(tb,nchanl, &
               iclwflg=4
            end if
         end if
-
-!  clw bias correction        
-        clw=clw+1.1
-        if(clw>6.0_r_kind) clw = zero
-     end if
-
-  end if
+    end if
+!  clw qc
+   if(clw>6.0_r_kind) clw = 6.0_r_kind
+   if(clw < 0.0_r_kind) clw = 0.0_r_kind
 
   return
 end subroutine ret_ssmis
 
-
-
-subroutine ice_check(tbx,seaice,ierr)
-
-!$$$ subprogram documentation block
-!
-! subprogram:    ice_check 
-!
-! prgmmr: weng, okamoto     org: np23                date: 2005-03-22
-!
-! abstract: using imager to estimate sea ice concentration 
-!
-! program history log:
-!     2005-03-22  okamoto
-!     2008-04-16  safford - rm unused uses
-!
-!   input argument list:
-!     tbx      - coefficients for calculation of seaice 
-!
-!   output argument list:
-!     seaice     - sea ice concentration 
-!     ierr    - error flag
-!
-! attributes:
-!     language: f90
-!     machine:  ibm RS/6000 SP
-!
-!$$$ end documentation block
-  use kinds, only: r_kind, i_kind
-  use constants, only: zero
-  
-  implicit none
-  
-  real(r_kind),dimension(7),intent(in):: tbx
-  integer(i_kind),intent(out)         :: ierr
-  real(r_kind),intent(out)            :: seaice
-  
-  ierr = zero 
-  
-  seaice = 91.9_r_kind - 2.994_r_kind*tbx(3) + 2.846_r_kind*tbx(1)  &
-       - 0.386_r_kind*tbx(4) + 0.495_r_kind*tbx(6) + 1.005_r_kind*tbx(2) &
-       - 0.904_r_kind*tbx(5)
-  
-  if( seaice>70.0_r_kind ) ierr = 3
-  
-  return
-end subroutine ice_check
