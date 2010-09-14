2dec7f552b7c5d9c96904011826e81349752b780
daryl.kleist@noaa.gov <daryl.kleist@noaa.gov>
2010-09-14 19:04:49 +0000

    EXP-hybens: bug fix and ability to specify tcp oberr through namelist    1. Fix in qnewton.f90 module to allow for compile on 'jet'    2. Ob error for tcp is deflated as a fucntion of magnitude of O-F.       This was hardwired in read_tcps and setuptcp, but has now been made       more flexible:
          - Three namelist parameters to control ob error for these obs have
            been added (for the ob error itself, as well as two parameters to 
            control the linear deflation as O-F increases.  
    
          - This is to address problems that have been noted in some EnKF runs
            which were using the default error values for tcp obs (creating 
            much too large of increments in vicinity of strong storms).

--- src/setuptcp.f90
+++ src/setuptcp.f90
@@ -38,6 +38,7 @@ subroutine setuptcp(lunin,mype,bwork,awork,nele,nobs,is,conv_diagsave)
           icsubtype
   use jfunc, only: jiter,last,jiterstart,miter
   use m_dtime, only: dtime_setup, dtime_check, dtime_show
+  use tcv_mod, only: tcp_innmax,tcp_oedelt
   implicit none
 
   integer(i_kind)                                  ,intent(in   ) :: lunin,mype,nele,nobs
@@ -263,13 +264,13 @@ subroutine setuptcp(lunin,mype,bwork,awork,nele,nobs,is,conv_diagsave)
      endif
 
 ! Redefine observation error basied on residual
-! inflate the ob error linearly as residual increases (max of O-F of 20 mb), 
+! inflate the ob error linearly as residual increases (max of O-F of 'tcp_innmax' mb), 
      error_orig = error
-     alpha = min((abs(r10*ddiff)/20.0_r_kind),one)
+     alpha = min((abs(r10*ddiff)/tcp_innmax),one)
 
 ! Here is the error factor added, much like 'drdp'
-! It varies from 0 to 1.5 (so ob error varies from 0.75 to 2.25 with this)
-     resfct = alpha*1.5_r_kind/r10
+! It varies from 0 to tcp_oedelt (so ob error varies from tcp_oberr to 'tcp_oberr+tcp_oedelt' with this)
+     resfct = alpha*tcp_oedelt/r10
 
 ! observational error adjustment
      drdp = pges*(g_over_rd*abs(rdelz)*drbx/(tges**2))
