c934b6deba9adcd767a8941bbcb255e8576412d2
ming.hu@noaa.gov <ming.hu@noaa.gov>
2012-04-17 15:45:24 +0000

    Change gsdpbl_height to pbl_height in subroutine tune_pbl_height.

--- src/tune_pbl_height.f90
+++ src/tune_pbl_height.f90
@@ -1,7 +1,7 @@
-subroutine tune_gsdpbl_height(mype,station_id,dx,dy,prestsfc,thisPBL_height,diffsfc)
+subroutine tune_pbl_height(mype,station_id,dx,dy,prestsfc,thisPBL_height,diffsfc)
 !$$$  subprogram documentation block
 !                .      .    .                                       .
-! subprogram:    tune_gsdpbl_height
+! subprogram:    tune_pbl_height
 !   prgmmr: Ming Hu          org: GSD                date: 2011-06-22
 !
 ! abstract: tune PBL height based on surface temperature residue
@@ -30,7 +30,7 @@ subroutine tune_gsdpbl_height(mype,station_id,dx,dy,prestsfc,thisPBL_height,diff
   use kinds, only: r_kind,r_single,r_double,i_kind
   use gridmod, only: istart,jstart,nlon,nlat,lon1,lon2,lat2
   use constants, only: zero, one,rd_over_cp,ten
-  use guess_grids, only: gsdpbl_height, ges_ps
+  use guess_grids, only: pbl_height, ges_ps
   implicit none
 
 !
@@ -78,14 +78,14 @@ subroutine tune_gsdpbl_height(mype,station_id,dx,dy,prestsfc,thisPBL_height,diff
 
   itime=1
   thisPBL_height           = prestsfc - max(zero,pblfact_cool*(prestsfc-thisPBL_height))
-  gsdpbl_height(ix,iy,itime)  = ges_ps(ix,iy,itime)*ten - max(zero, &
-                             pblfact_cool*(ges_ps(ix,iy,itime)*ten-gsdpbl_height(ix,iy,itime)))
-  gsdpbl_height(ixp,iy,itime) = ges_ps(ixp,iy,itime)*ten - max(zero,& 
-                             pblfact_cool*(ges_ps(ixp,iy,itime)*ten-gsdpbl_height(ixp,iy,itime)))
-  gsdpbl_height(ix,iyp,itime) = ges_ps(ix,iyp,itime)*ten - max(zero,&
-                             pblfact_cool*(ges_ps(ix,iyp,itime)*ten-gsdpbl_height(ix,iyp,itime)))
-  gsdpbl_height(ixp,iyp,itime)= ges_ps(ixp,iyp,itime)*ten - max(zero,&
-                             pblfact_cool*(ges_ps(ixp,iyp,itime)*ten-gsdpbl_height(ixp,iyp,itime)))
+  pbl_height(ix,iy,itime)  = ges_ps(ix,iy,itime)*ten - max(zero, &
+                             pblfact_cool*(ges_ps(ix,iy,itime)*ten-pbl_height(ix,iy,itime)))
+  pbl_height(ixp,iy,itime) = ges_ps(ixp,iy,itime)*ten - max(zero,& 
+                             pblfact_cool*(ges_ps(ixp,iy,itime)*ten-pbl_height(ixp,iy,itime)))
+  pbl_height(ix,iyp,itime) = ges_ps(ix,iyp,itime)*ten - max(zero,&
+                             pblfact_cool*(ges_ps(ix,iyp,itime)*ten-pbl_height(ix,iyp,itime)))
+  pbl_height(ixp,iyp,itime)= ges_ps(ixp,iyp,itime)*ten - max(zero,&
+                             pblfact_cool*(ges_ps(ixp,iyp,itime)*ten-pbl_height(ixp,iyp,itime)))
   write(*,'(a,a8,10f10.2)') 'tuned PBL height=', station_id,diffsfc,pblfact_cool,prestsfc,thisPBL_height,oldPBL_height 
 
-end subroutine tune_gsdpbl_height
+end subroutine tune_pbl_height
