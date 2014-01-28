a4e75e3372e9ac73b87190f365ca39a0c6aafac6
russ.treadon <russ.treadon@noaa.gov>
2014-01-28 15:16:14 +0000

    EXP-trunk:  add preliminary pieces of Ozone Monitoring package (see ticket #362)

---
+++ util/Ozone_Monitor/scripts/update.sh
@@ -0,0 +1,34 @@
+#! /bin/ksh
+
+#. ~/.bashrc
+
+set -ax
+export list=$listvar
+
+if [ $machine = WCOSS ]; then
+   source ~/.bashrc
+fi
+
+#------------------------------------------------------------------
+# Set environment variables.
+tmpdir=${stmproot}/${LOGNAME}/done_${SUFFIX}_${string}.$PDATE
+rm -rf $tmpdir
+mkdir -p $tmpdir
+cd $tmpdir
+
+
+#--------------------------------------------------------------------
+# Set next date to process
+qdate=`$NDATE +06 $PDATE`
+echo $qdate > ./dum
+$NCP ./dum $TANKDIR/cycle/prodate
+
+
+#--------------------------------------------------------------------
+# Clean up and exit
+cd $tmpdir
+cd ../
+rm -rf $tmpdir
+
+exit
+ 
