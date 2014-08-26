606e0927f546e52300ef014937b5bee3315a02d1
xiujuan.su@noaa.gov <xiujuan.su@noaa.gov>
2014-08-26 14:10:04 +0000

    Removed some commented lines and some mistakes.

--- src/read_satwnd.f90
+++ src/read_satwnd.f90
@@ -701,11 +701,6 @@ subroutine read_satwnd(nread,ndata,nodata,infile,obstype,lunout,gstime,twind,sis
                  c_prvstg='AVHRR'
                  if(hdrdat(9) == one)  then                            ! IR winds
                     itype=244
-!                   The following two lines have been commented out because they 
-!                   automatically disallow the AVHRR observations and we don't 
-!                   want that - should be controlled at satinfo level.
-!                   qm=15
-!                   pqm=15
                  else
                     write(6,*) 'READ_SATWND: wrong derived method value'
                  endif
@@ -716,9 +711,9 @@ subroutine read_satwnd(nread,ndata,nodata,infile,obstype,lunout,gstime,twind,sis
                     if(qcdat(2,j) <= r10000 .and. qcdat(3,j) <r10000 ) then
                        if(qcdat(2,j) ==  one  .and. qifn >r105) then
                           qifn=qcdat(3,j)
-                       else if(qcdat(2,j) ==  three .and. qify >105) then
+                       else if(qcdat(2,j) ==  three .and. qify >r105) then
                           qify=qcdat(3,j)
-                       else if( qcdat(2,j) == four .and. ee >105) then
+                       else if( qcdat(2,j) == four .and. ee >r105) then
                           ee=qcdat(3,j)
                        endif 
                     endif
@@ -730,7 +725,6 @@ subroutine read_satwnd(nread,ndata,nodata,infile,obstype,lunout,gstime,twind,sis
                  if(hdrdat(10) >68.0_r_kind) cycle loop_readsb   !   reject data zenith angle >68.0 degree 
                  if(hdrdat(9) == one)  then                            ! short wave IR winds
                     itype=240
-!                    qm=15
                     c_station_id='IR'//stationid
                     c_sprvstg='IR'
                  endif
@@ -741,9 +735,9 @@ subroutine read_satwnd(nread,ndata,nodata,infile,obstype,lunout,gstime,twind,sis
                     if(qcdat(2,j) <= r10000 .and. qcdat(3,j) <r10000 ) then
                        if(qcdat(2,j) ==  one  .and. qifn >r105) then
                           qifn=qcdat(3,j)
-                       else if(qcdat(2,j) ==  three .and. qify >105) then
+                       else if(qcdat(2,j) ==  three .and. qify >r105) then
                           qify=qcdat(3,j)
-                       else if( qcdat(2,j) == four .and. ee >105) then
+                       else if( qcdat(2,j) == four .and. ee >r105) then
                           ee=qcdat(3,j)
                        endif
                     endif
@@ -754,7 +748,6 @@ subroutine read_satwnd(nread,ndata,nodata,infile,obstype,lunout,gstime,twind,sis
                  c_prvstg='VIIRS'
                  if(hdrdat(9) == one)  then                            ! VIIRS IR winds
                     itype=260
-!                    qm=15
                     c_station_id='IR'//stationid
                     c_sprvstg='IR'
                  endif
@@ -765,9 +758,9 @@ subroutine read_satwnd(nread,ndata,nodata,infile,obstype,lunout,gstime,twind,sis
                     if(qcdat(2,j) <= r10000 .and. qcdat(3,j) <r10000 ) then
                        if(qcdat(2,j) ==  one  .and. qifn >r105) then
                           qifn=qcdat(3,j)
-                       else if(qcdat(2,j) ==  three .and. qify >105) then
+                       else if(qcdat(2,j) ==  three .and. qify >r105) then
                           qify=qcdat(3,j)
-                       else if( qcdat(2,j) == four .and. ee >105) then
+                       else if( qcdat(2,j) == four .and. ee >r105) then
                           ee=qcdat(3,j)
                        endif
                     endif
@@ -862,7 +855,7 @@ subroutine read_satwnd(nread,ndata,nodata,infile,obstype,lunout,gstime,twind,sis
             if(itype==245 .or. itype==246) then
 !               obserr=obserr*two
 !  using  Santek quality control method,calculate the original ee value
-               if(ee <105.0_r_kind) then
+               if(ee <r105) then
                   ree=(ee-r100)/(-10.0_r_kind)
                   if(obsdat(4) >zero) then
                     ree=ree/obsdat(4)
