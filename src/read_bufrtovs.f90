037ebdba63226818bdae82618718676f3a778b94
david.groff@noaa.gov <david.groff@noaa.gov>
2014-09-04 20:40:23 +0000

    EXP-testCRTM_R2.2 branch. Merged trunk 43655:44894 into the EXP-testCRTM_R2.2 branch.

--- src/read_bufrtovs.f90
+++ src/read_bufrtovs.f90
@@ -124,10 +124,10 @@ subroutine read_bufrtovs(mype,val_tovs,ithin,isfcalc,&
   use radinfo, only: crtm_coeffs_path,adp_anglebc
   use gridmod, only: diagnostic_reg,regional,nlat,nlon,tll2xy,txy2ll,rlats,rlons
   use constants, only: deg2rad,zero,one,two,three,five,rad2deg,r60inv,r1000,h300
-  use crtm_module, only: crtm_destroy,crtm_init,success,crtm_channelinfo_type, &
+  use crtm_module, only: success, &
       crtm_kind => fp, &
       MAX_SENSOR_ZENITH_ANGLE
-  use crtm_spccoeff, only: sc
+  use crtm_spccoeff, only: sc,crtm_spccoeff_load,crtm_spccoeff_destroy
   use calc_fov_crosstrk, only : instrument_init, fov_cleanup, fov_check
   use gsi_4dvar, only: l4dvar,iwinbgn,winlen
   use antcorr_application, only: remove_antcorr
@@ -186,7 +186,6 @@ subroutine read_bufrtovs(mype,val_tovs,ithin,isfcalc,&
   integer(i_kind) radedge_min, radedge_max
   integer(i_kind),allocatable,dimension(:)::nrec
   character(len=20),dimension(1):: sensorlist
-  type(crtm_channelinfo_type),dimension(1) :: channelinfo
 
   real(r_kind) cosza,sfcr
   real(r_kind) ch1,ch2,ch3,ch8,d0,d1,d2,ch15,qval
@@ -477,18 +476,14 @@ subroutine read_bufrtovs(mype,val_tovs,ithin,isfcalc,&
         allocate(data1b8x(nchanl))
         sensorlist(1)=sis
         if( crtm_coeffs_path /= "" ) then
-           if(mype_sub==mype_root) write(6,*)'READ_BUFRTOVS: crtm_init() on path "'//trim(crtm_coeffs_path)//'"'
-           error_status = crtm_init(sensorlist,channelinfo,&
-              Process_ID=mype_sub,Output_Process_ID=mype_root, &
-              Load_CloudCoeff=.FALSE.,Load_AerosolCoeff=.FALSE., &
+           if(mype_sub==mype_root) write(6,*)'READ_BUFRTOVS: crtm_spccoeff_load() on path "'//trim(crtm_coeffs_path)//'"'
+           error_status = crtm_spccoeff_load(sensorlist,&
               File_Path = crtm_coeffs_path )
            else
-              error_status = crtm_init(sensorlist,channelinfo,&
-                 Process_ID=mype_sub,Output_Process_ID=mype_root,&
-                 Load_CloudCoeff=.FALSE.,Load_AerosolCoeff=.FALSE.)
+              error_status = crtm_spccoeff_load(sensorlist)
            endif
            if (error_status /= success) then
-              write(6,*)'READ_BUFRTOVS:  ***ERROR*** crtm_init error_status=',error_status,&
+              write(6,*)'READ_BUFRTOVS:  ***ERROR*** crtm_spccoeff_load error_status=',error_status,&
                  '   TERMINATE PROGRAM EXECUTION'
            call stop2(71)
         endif
@@ -887,9 +882,9 @@ subroutine read_bufrtovs(mype,val_tovs,ithin,isfcalc,&
 
   if(llll == 2)then
 ! deallocate crtm info
-     error_status = crtm_destroy(channelinfo)
+     error_status = crtm_spccoeff_destroy()
      if (error_status /= success) &
-        write(6,*)'OBSERVER:  ***ERROR*** crtm_destroy error_status=',error_status
+        write(6,*)'OBSERVER:  ***ERROR*** crtm_spccoeff_destroy error_status=',error_status
   end if
 
 !   Jump here when there is a problem opening the bufr file
