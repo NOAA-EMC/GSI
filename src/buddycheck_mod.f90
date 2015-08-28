329a39b6d401c35592b623dab4ae98682485babf
runhua.yang <runhua.yang@noaa.gov>
2015-08-28 15:01:41 +0000

    Change value of difmax to 7.0 in temperature buddy check

--- src/buddycheck_mod.f90
+++ src/buddycheck_mod.f90
@@ -323,7 +323,7 @@ subroutine buddy_check_t(is,data,luse,mype,nele,nobs,muse,buddyuse)
   call final_vars_
   ! - Now call buddy check routine
   range = 108000.0_r_kind ! Radius within which we check for an ob's buddies (units are m)
-  difmax= 8.0_r_kind      ! Max difference allowed relative to buddies
+  difmax= 7.0_r_kind      ! Max difference allowed relative to buddies
  
   call execute_buddy_check(mype,is,nobs,vals,range,difmax,buddyuse)
 
