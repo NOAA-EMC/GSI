99ad3d0df1f06adefb13c0d52754841f0a14226c
runhua.yang <runhua.yang@noaa.gov>
2015-09-29 19:12:39 +0000

    comment out a printing in read_prepbufr_uv.

--- src/read_prepbufr.f90
+++ src/read_prepbufr.f90
@@ -1147,7 +1147,7 @@ subroutine read_prepbufr(nread,ndata,nodata,infile,obstype,lunout,twindin,sis,&
                       endif
                       del_uv=max(zero,min(del_uv,one))
 ! Wind error
-                         write(6,*) 'READ_PREPBUFR_UV:',itypex,k1_uv,itypey,k2_uv,ierr_uv,nc,kx,ppb
+!                         write(6,*) 'READ_PREPBUFR_UV:',itypex,k1_uv,itypey,k2_uv,ierr_uv,nc,kx,ppb
                          obserr(5,k)=(one-del_uv)*etabl_uv(itypex,k1_uv,ierr_uv)+del_uv*etabl_uv(itypex,k2_uv,ierr_uv)
                          obserr(5,k)=max(obserr(5,k),werrmin)
 !Wind b
