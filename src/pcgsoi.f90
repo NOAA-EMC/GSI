97302af9d9d7c3be14aab606d443706363ce3021
david.parrish <david.parrish@noaa.gov>
2016-05-09 18:26:52 +0000

    EXP-betaprofile changes:  (1) fix -- modify nam_hybens_d01_info to be constant in vertical, add constant profiles hwrf_hybens_d2_info, hwrf_hybens_d3_info ::: (2) scripts -- modify hwrf_nmm_d2.sh, hwrf_nmm_d3.sh, nmmb_nems_4denvar.sh, regression_namelists.sh, regression_namelists_db.sh to allow use of readin_beta namelist parameter ::: (3) src -- change pcgsoi.f90, hybrid_ensemble_isotropic.F90, bkerror.f90 so beta12mult is removed from pcgsoi, and new routine beta_s_mult is called in bkerror.f90 and beta_e_mult is called in bkerror_a_ens.  This change produces bit-wise identical results in regression tests.  Next change will be to replace beta_s_mult, beta_e_mult with sqrt_beta_s_mult, sqrt_beta_e_mult called at beginning and end of bkerror and bkerror_a_ens routines.  This will preserve background and localization error symmetry for variable in vertical beta weights.

--- src/pcgsoi.f90
+++ src/pcgsoi.f90
@@ -102,6 +102,8 @@ subroutine pcgsoi()
 !                          by replacing mval with mval(1).  This is likely not
 !                          correct for multiple obs bins.
 !   2014-12-22  Hu      -  add option i_gsdcldanal_type to control cloud analysis  
+!   2016-05-09  parrish -  remove beta12mult.  Replace with beta_s_mult, beta_e_mult, inside
+!                            bkerror and bkerror_a_en
 !                       
 !
 ! input argument list:
@@ -147,7 +149,7 @@ subroutine pcgsoi()
   use projmethod_support, only: init_mgram_schmidt, &
                                 mgram_schmidt,destroy_mgram_schmidt
   use hybrid_ensemble_parameters,only : l_hyb_ens,aniso_a_en,ntlevs_ens
-  use hybrid_ensemble_isotropic, only: beta12mult,bkerror_a_en
+  use hybrid_ensemble_isotropic, only: bkerror_a_en
   use gsi_bundlemod, only : gsi_bundle
   use gsi_bundlemod, only : self_add,assignment(=)
   use gsi_bundlemod, only : gsi_bundleprint
@@ -381,8 +383,6 @@ subroutine pcgsoi()
 !       multiply static (Jb) part of grady by betas_inv(:), and
 !       multiply ensemble (Je) part of grady by betae_inv(:) [default : betae_inv(:) = 1 - betas_inv(:)] 
 !         (this determines relative contributions from static background Jb and ensemble background Je)
-
-        call beta12mult(grady)
 
      end if
 
@@ -708,8 +708,6 @@ subroutine pcgsoi()
 !    multiply static (Jb) part of grady by betas_inv(:), and
 !    multiply ensemble (Je) part of grady by betae_inv(:). [Default : betae_inv(:) =  1 - betas_inv(:) ]
 !      (this determines relative contributions from static background Jb and ensemble background Je)
-
-       call beta12mult(grady)
 
      end if
 
