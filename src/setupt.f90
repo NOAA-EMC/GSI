c36544ba4402b28635ae9d6c65ad81ea9dd932da
yanqiu.zhu@noaa.gov <yanqiu.zhu@noaa.gov>
2016-08-24 18:09:01 +0000

    tic591_dtfinc_mask: minor changes of if conditions

--- src/setupt.f90
+++ src/setupt.f90
@@ -448,7 +448,7 @@ subroutine setupt(lunin,mype,bwork,awork,nele,nobs,is,conv_diagsave)
      end if
 
 !    aircraftobst = itype>129.and.itype<140
-     aircraftobst = (itype==131) .or. (itype>=133) .or. (itype==130)
+     aircraftobst = (itype==131) .or. (itype>=133 .and. itype<=135) .or. (itype==130) !for currently known types
      ix = 0
      if (aircraftobst .and. (aircraft_t_bc_pof .or. aircraft_t_bc .or. aircraft_t_bc_ext)) then 
         ix = data(idx,i)
