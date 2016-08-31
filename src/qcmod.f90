a02fd30b5c054dc6cc5a82d99c6e3fbb3bde72df
Michael.Lueken <Michael.Lueken@noaa.gov>
2016-08-31 17:36:25 +0000

    tic591_dtfinc_mask: Merged EXP-NESDIS-JCSDA_r2o at r81075 to tic591_dtfinc_mask branch in preparation for submittal to review committee.

--- src/qcmod.f90
+++ src/qcmod.f90
@@ -50,8 +50,9 @@ module qcmod
 !                         cloud info, diff_clw, scattering and surface wind
 !                         speed for AMSUA/ATMS cloudy radiance assimilation
 !   2015-05-01  ejones  - modify emissivity regression and check in qc_gmi
-!   2015-05-29  ejones  - tighten clw threshold for qc_gmi 
 !   2015-09-04  J.Jung  - Added mods for CrIS full spectral resolution (FSR)
+!   2015-05-29  ejones  - tighten clw threshold for qc_gmi 
+!   2015-09-30  ejones  - add sun glint check in qc_amsr2 
 !
 ! subroutines included:
 !   sub init_qcvars
@@ -216,10 +217,14 @@ module qcmod
   integer(i_kind),parameter:: ifail_scanedge_qc=51
 !  Reject S1 swath edges
   integer(i_kind),parameter:: ifail_gmi_swathedge_qc=52
+!  Reject if latitude is outside of 55N - 55S
+  integer(i_kind),parameter:: ifail_lat_qc=53
 
 ! QC_AMSR2 failures
 !  Reject due to krain type not equal to 0 in subroutine qc_amsr2
   integer(i_kind),parameter:: ifail_krain_amsr2_qc=50
+!  Reject due to sun glint in subroutine qc_amsr2
+  integer(i_kind),parameter:: ifail_amsr2_glint_qc=51
 
 ! QC_SAPHIR failures
 !  Reject due to krain type not equal to 0 in subroutine qc_saphir
@@ -1095,7 +1100,7 @@ subroutine qc_ssmi(nchanl,nsig,ich,sfchgt,luse,sea,mixed, &
   return
 end subroutine qc_ssmi
 
-subroutine qc_gmi(nchanl,sfchgt,luse,sea, &
+subroutine qc_gmi(nchanl,sfchgt,luse,sea,cenlat, &
      kraintype,clw,tsavg5,tbobs,gmi,varinv,aivals,id_qc)
 !$$$ subprogram documentation block
 !               .      .    .
@@ -1111,12 +1116,14 @@ subroutine qc_gmi(nchanl,sfchgt,luse,sea, &
 !     2015-01-16  ejones  - copied and modified qc_ssmi
 !     2015-02-13  ejones  - added swath edge check
 !     2015-02-17  ejones  - added emissivity regression and check
+!     2016-05-05  ejones  - added check for latitudes above/below 55N/S
 !
 ! input argument list:
 !     nchanl       - number of channels per obs
 !     sfchgt  - surface height (not use now)
 !     luse    - logical use flag
 !     sea     - logical, sea flag
+!     cenlat   - latitude of observation
 !     kraintype - [0]no rain, [others]rain ; see retrieval_mi
 !     clw     - retrieve clw [kg/m2]
 !     tsavg5       - surface skin temperature
@@ -1146,6 +1153,7 @@ subroutine qc_gmi(nchanl,sfchgt,luse,sea, &
   logical                          ,intent(in   ) :: gmi
 
   real(r_kind)                     ,intent(in   ) :: sfchgt,clw,tsavg5
+  real(r_kind)                     ,intent(in   ) :: cenlat
   real(r_kind)   ,dimension(nchanl),intent(in   ) :: tbobs
 
   real(r_kind)   ,dimension(nchanl),intent(inout) :: varinv
@@ -1282,6 +1290,14 @@ subroutine qc_gmi(nchanl,sfchgt,luse,sea, &
        end do
     end if
 
+    ! check latitude. If obs is south of 55S or north of 55N, don't use it; it
+    ! may be affected by sea ice.
+    if (abs(cenlat)>55.0_r_kind) then
+       do i=1,13
+          varinv(1:13)=zero
+          if (id_qc(i) == igood_qc) id_qc(i)=ifail_lat_qc
+       end do
+    end if
 
 !    Use data not over over sea
   else  !land,sea ice,mixed
@@ -1315,7 +1331,7 @@ subroutine qc_gmi(nchanl,sfchgt,luse,sea, &
 end subroutine qc_gmi
 
 subroutine qc_amsr2(nchanl,sfchgt,luse,sea, &
-     kraintype,clw,amsr2,varinv,aivals,id_qc)
+     kraintype,clw,tsavg5,tbobs,solazi,solzen,amsr2,varinv,aivals,id_qc)
 !$$$ subprogram documentation block
 !               .      .    .
 ! subprogram:  qc_amsr2     QC for amsr2 TBs
@@ -1328,6 +1344,12 @@ subroutine qc_amsr2(nchanl,sfchgt,luse,sea, &
 !
 ! program history log:
 !     2015-01-16  ejones
+!     2015-10-02  ejones   - add emissivity retrievals and checks, update clw
+!                            check, add sun glint check
+!     2015-10-07  ejones   - add bias correction to TBs prior to emissivity
+!                            retrievals
+!     2015-11-09  ejones   - add an extra check to Ch1 TBs for observations
+!                            missed by the CLW check that are probably cloudy
 !
 ! input argument list:
 !     nchanl       - number of channels per obs
@@ -1337,6 +1359,10 @@ subroutine qc_amsr2(nchanl,sfchgt,luse,sea, &
 !     kraintype - [0]no rain, [others]rain ; see retrieval_mi
 !     clw     - retrieve clw [kg/m2]
 !     amsr2   - logical true if gmi is processed
+!     solazi  - solar azimuth angle
+!     solzen  - solar zenith angle
+!     tbobs   - brightness temperatures
+!     tsavg5  - skin temp
 !
 ! output argument list:
 !     varinv  - observation weight (modified obs var error inverse)
@@ -1363,19 +1389,32 @@ subroutine qc_amsr2(nchanl,sfchgt,luse,sea, &
   real(r_kind)                     ,intent(in   ) :: sfchgt,clw
   real(r_kind)   ,dimension(nchanl),intent(inout) :: varinv
   real(r_kind)   ,dimension(40)    ,intent(inout) :: aivals
+  real(r_kind)                     ,intent(in   ) :: solazi,solzen
+  real(r_kind)   ,dimension(nchanl),intent(in   ) :: tbobs
+  real(r_kind)                     ,intent(in   ) :: tsavg5
 
 ! Declare local variables
   integer(i_kind) :: l,i
   real(r_kind) :: efact,vfact,fact
+
+  real(r_kind) :: solel, solazi_rad, solel_rad
+  real(r_kind) :: ang, ang_a, ang_b, ang_ab
   real(r_kind),dimension(nchanl) :: clwcutofx
+
+  integer(i_kind)                :: idx, nch_emrgr
+  integer(i_kind),dimension(nchanl) :: idxch_emrgr
+  real(r_kind),dimension(nchanl) :: rgr_coeff_36h, rgr_coeff_89h, sys_bias, tb_use
+  real(r_kind),dimension(2)      :: rgr_coeff2_36h, rgr_coeff2_89h
+  real(r_kind) :: c36h, c89h, d36h, d89h
+  real(r_kind) :: em36h, em89h, em2_36h, em2_89h, diff_em_36h, diff_em_89h
 
 !------------------------------------------------------------------
 ! Set cloud qc criteria  (kg/m2) :  reject when clw>clwcutofx
   if (amsr2) then
      clwcutofx(1:nchanl) =  &
-         (/ 0.350_r_kind, 0.350_r_kind, 0.350_r_kind, 0.350_r_kind, 0.350_r_kind, &
-            0.350_r_kind, 0.350_r_kind, 0.350_r_kind, 0.270_r_kind, 0.270_r_kind, &
-            0.100_r_kind, 0.100_r_kind, 0.050_r_kind, 0.050_r_kind /)
+         (/ 0.050_r_kind, 0.050_r_kind, 0.050_r_kind, 0.050_r_kind, 0.050_r_kind, &
+            0.050_r_kind, 0.050_r_kind, 0.050_r_kind, 0.050_r_kind, 0.050_r_kind, &
+            0.050_r_kind, 0.050_r_kind, 0.050_r_kind, 0.050_r_kind /)
   endif
 
 ! Loop over observations.
@@ -1418,9 +1457,122 @@ subroutine qc_amsr2(nchanl,sfchgt,luse,sea, &
         end do  !l_loop
      end if
 
-!   Calculate emissivity and flag observations over thresholds
-!   Need to work on emissivity regression
+! flag points where channel 1 tbs > 200K, these are probably cloud
 
+     if( tbobs(1) > 200.0_r_kind ) then
+        do l=1,nchanl
+           varinv(l)=zero
+           if(luse) then
+              aivals(10) = aivals(10) + one
+              if(id_qc(l)== igood_qc) then
+                 id_qc(l)=ifail_cloud_qc
+                 aivals(9)=aivals(9) + one
+              end if
+           end if
+        end do
+     end if
+
+
+! calculate and flag sun glint
+    solel = 90.0_r_kind-solzen
+    solazi_rad = solazi*deg2rad
+    solel_rad = solel*deg2rad
+
+    ang = atan(solel_rad/solazi_rad)
+    ang_a = ( (solazi*cos(ang)) - (solel*sin(ang)) )
+    ang_b = ( (solazi*sin(ang)) + (solel*cos(ang)) )
+    ang_ab = sqrt(ang_a**2 + ang_b**2)
+
+! only flag first 6 channels for sun glint    
+!    do l=1,nchanl
+    do l=1,6             
+       if (ang_ab < 26.0_r_kind) then
+         varinv(l)=zero
+         if(luse) then
+            if(id_qc(l)== igood_qc) then
+               id_qc(l)=ifail_amsr2_glint_qc
+            endif
+         endif
+       endif 
+    enddo
+      
+!   Calculate emissivity and flag observations over thresholds
+!   Calculations for ch 3,4,5
+    nch_emrgr = 14
+    idxch_emrgr = (/1,2,3,4,5,6,7,8,9,10,11,12,13,14/)
+ 
+    ! Brightness temperatures used for training emissivity retrievals were
+    ! simulated from ECMWF fields collocated with AMSR2 observations. The retrievals
+    ! here use actual GMI brightness temperatures, so for best results, a
+    ! "systematic bias" (i.e. an average difference between AMSR2 brightness
+    ! temperatures and those simulated from ECMWF fields) is removed from AMSR2
+    ! brightness temperatures prior to performing retrievals
+
+    ! systematic bias
+    sys_bias= (/ 0.4800_r_kind, 3.0737_r_kind, 0.7433_r_kind, 3.6430_r_kind,&
+                 3.5304_r_kind, 4.4270_r_kind, 5.1448_r_kind, 5.0785_r_kind,&
+                 4.9763_r_kind, 9.3215_r_kind, 2.5789_r_kind, 5.5274_r_kind,&
+                 0.6641_r_kind, 1.3674_r_kind /)
+
+    ! brightness temperatures to use
+    tb_use(1)=(tbobs(1)-sys_bias(1)); tb_use(2)=(tbobs(2)-sys_bias(2)); tb_use(3)=(tbobs(3)-sys_bias(3))
+    tb_use(4)=(tbobs(4)-sys_bias(4)); tb_use(5)=(tbobs(5)-sys_bias(5)); tb_use(6)=(tbobs(6)-sys_bias(6))
+    tb_use(7)=(tbobs(7)-sys_bias(7)); tb_use(8)=(tbobs(8)-sys_bias(8)); tb_use(9)=(tbobs(9)-sys_bias(9))
+    tb_use(10)=(tbobs(10)-sys_bias(10)); tb_use(11)=(tbobs(11)-sys_bias(11)); tb_use(12)=(tbobs(12)-sys_bias(12))
+    tb_use(13)=(tbobs(13)-sys_bias(13)); tb_use(14)=(tbobs(14)-sys_bias(14))
+
+    ! Set regression constants and coefficients
+    ! first set of constants and coefficients (using all channels)
+    c36h = 1.18467_r_kind
+    c89h = 1.73315_r_kind
+
+    rgr_coeff_36h = (/ -0.00098_r_kind, 0.00145_r_kind, -0.00146_r_kind, 0.00055_r_kind, &
+                       -0.00232_r_kind, 0.00061_r_kind, 0.00160_r_kind, 0.00001_r_kind, &
+                       -0.00053_r_kind, -0.00019_r_kind, -0.00272_r_kind, 0.00104_r_kind, &
+                       -0.00026_r_kind, 0.00032_r_kind /)
+    rgr_coeff_89h = (/ -0.00141_r_kind, 0.00217_r_kind, -0.00214_r_kind, 0.00070_r_kind, &
+                       -0.00358_r_kind, 0.00110_r_kind, 0.00199_r_kind, 0.00002_r_kind, &
+                       -0.00131_r_kind, 0.00003_r_kind, -0.00318_r_kind, 0.00122_r_kind, &
+                       -0.00043_r_kind, 0.00047_r_kind /)
+
+    ! second set of constants and coefficients (single channel regression)
+    d36h = 1.08529_r_kind
+    d89h = 1.66380_r_kind
+
+    rgr_coeff2_36h = (/ 0.00017_r_kind, -0.00269_r_kind /)
+    rgr_coeff2_89h = (/ 0.00017_r_kind, -0.00433_r_kind /)
+
+    ! perform regressions
+    ! first set
+    em36h = c36h
+    em89h = c89h
+    do i=1,nch_emrgr
+      idx=idxch_emrgr(i)
+      em36h=em36h+(tb_use(idx)*rgr_coeff_36h(i))    ! 36h multi-channel emiss
+      em89h=em89h+(tb_use(idx)*rgr_coeff_89h(i))    ! 89h multi-channel emiss
+    end do
+
+    ! second set, using tskin
+    ! 36h single-channel emiss
+    em2_36h = d36h + ( tb_use(12)*rgr_coeff2_36h(1) ) + ( rgr_coeff2_36h(2) * tsavg5 )
+    ! 36h single-channel emiss
+    em2_89h = d89h + ( tb_use(14)*rgr_coeff2_89h(1) ) + ( rgr_coeff2_89h(2) * tsavg5 )
+
+    ! calculate differences between emissivity regressions
+    ! single channel less multiple channel
+    diff_em_36h = em2_36h - em36h
+    diff_em_89h = em2_89h - em89h
+
+    ! check emissivity difference values against thresholds and assign flag if
+    ! needed
+!    if ( (diff_em_36h > 0.015_r_kind) .or. (diff_em_89h > 0.015_r_kind) ) then
+    if ( (diff_em_36h > 0.008_r_kind) .or. (diff_em_89h > 0.008_r_kind) .or. &
+         (diff_em_36h < -0.030_r_kind) .or. (diff_em_89h < -0.030_r_kind) ) then
+       do i=1,14
+          varinv(1:14)=zero
+          if (id_qc(i) == igood_qc) id_qc(i)=ifail_emiss_qc
+       end do
+    end if
 
 !    Use data not over over sea
   else  !land,sea ice,mixed
