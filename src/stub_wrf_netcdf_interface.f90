06b8d1637674c27fac780e9d4a814954ecca3035
mark.potts@noaa.gov <mark.potts@noaa.gov>
2017-01-26 18:13:55 +0000

    Updated to remove unused variable (this) in wrf_netcdf_interface and enable parallel builds with original Make system

--- src/stub_wrf_netcdf_interface.f90
+++ src/stub_wrf_netcdf_interface.f90
@@ -16,39 +16,35 @@ module convert_netcdf_mod
 use abstract_convert_netcdf_mod 
   type, extends(abstract_convert_netcdf_class) :: convert_netcdf_class
   contains
-    procedure, pass(this) :: convert_netcdf_mass => convert_netcdf_mass_dummy
-    procedure, pass(this) :: convert_netcdf_nmm  => convert_netcdf_nmm_dummy
-    procedure, pass(this) :: update_netcdf_mass  => update_netcdf_mass_dummy
-    procedure, pass(this) :: update_netcdf_nmm   => update_netcdf_nmm_dummy
+    procedure, nopass :: convert_netcdf_mass => convert_netcdf_mass_dummy
+    procedure, nopass :: convert_netcdf_nmm  => convert_netcdf_nmm_dummy
+    procedure, nopass :: update_netcdf_mass  => update_netcdf_mass_dummy
+    procedure, nopass :: update_netcdf_nmm   => update_netcdf_nmm_dummy
   end type convert_netcdf_class
 contains
-  subroutine convert_netcdf_mass_dummy(this)
+  subroutine convert_netcdf_mass_dummy
     
     implicit none
-    class(convert_netcdf_class), intent(inout) :: this 
   end subroutine convert_netcdf_mass_dummy
   
-  subroutine convert_netcdf_nmm_dummy(this,update_pint,ctph0,stph0,tlm0,guess)
+  subroutine convert_netcdf_nmm_dummy(update_pint,ctph0,stph0,tlm0,guess)
     use kinds, only: r_single,i_kind,r_kind
     implicit none
-    class(convert_netcdf_class), intent(inout) :: this 
     logical     ,intent(in   ) :: guess
     logical     ,intent(inout) :: update_pint
     real(r_kind),intent(  out) :: ctph0,stph0,tlm0
   
   end subroutine convert_netcdf_nmm_dummy
   
-  subroutine update_netcdf_mass_dummy(this)
+  subroutine update_netcdf_mass_dummy
   
     implicit none
-    class(convert_netcdf_class), intent(inout) :: this 
   
   end subroutine update_netcdf_mass_dummy
   
-  subroutine update_netcdf_nmm_dummy(this)
+  subroutine update_netcdf_nmm_dummy
   
     implicit none
-    class(convert_netcdf_class), intent(inout) :: this 
   
   end subroutine update_netcdf_nmm_dummy
 end module convert_netcdf_mod
