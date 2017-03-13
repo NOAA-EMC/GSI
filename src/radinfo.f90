bda2f1f483246a0d8a179a5b8ea482dbe9607828
russ.treadon <russ.treadon@noaa.gov>
2017-03-13 18:36:06 +0000

    radbc_safeguard: add safeguards to address 2017030706 operational GFS/GDAS GSI failure (see #637)
    scripts/
     * exglobal_analysis.sh.ecf - add USE_RADSTAT option, (default NO)
    
    src/
     * prad_bias.f90 - add safeguards to exclude tiny values from linear matrix 
     * radinfo.f90 - add safeguards to exclude tiny values from linear matrix

--- src/radinfo.f90
+++ src/radinfo.f90
@@ -145,6 +145,20 @@ module radinfo
   real(r_kind),allocatable,dimension(:,:):: cbias     ! angle dependent bias for satellite channels
   real(r_kind),allocatable,dimension(:,:):: predx     ! coefficients for predictor part of bias correction
 
+! radiance bias correction terms are as follows:
+!  pred(1,:)  = global offset
+!  pred(2,:)  = zenith angle predictor, is not used and set to zero now
+!  pred(3,:)  = cloud liquid water predictor for clear-sky microwave radiance assimilation
+!  pred(4,:)  = square of temperature laps rate predictor
+!  pred(5,:)  = temperature laps rate predictor
+!  pred(6,:)  = cosinusoidal predictor for SSMI/S ascending/descending bias
+!  pred(7,:)  = sinusoidal predictor for SSMI/S
+!  pred(8,:)  = emissivity sensitivity predictor for land/sea differences
+!  pred(9,:)  = fourth order polynomial of angle bias correction
+!  pred(10,:) = third order polynomial of angle bias correction
+!  pred(11,:) = second order polynomial of angle bias correction
+!  pred(12,:) = first order polynomial of angle bias correction
+ 
   real(r_kind),allocatable,dimension(:,:):: varA
   real(r_kind),allocatable,dimension(:):: ostats
   real(r_quad),allocatable,dimension(:,:):: rstats
@@ -1504,6 +1518,7 @@ contains
    integer(i_kind),parameter:: nthreshold = 100
    integer(i_kind),parameter:: maxchn = 3000
    integer(i_kind),parameter:: maxdat = 100
+   real(r_kind),   parameter:: atiny  = 1.0e-10_r_kind
 
 !  Declare local variables
    logical lexist
@@ -1527,7 +1542,7 @@ contains
    integer(i_kind),dimension(maxchn):: ich
    integer(i_kind),dimension(maxdat):: ipoint
  
-   real(r_kind):: bias,scan,errinv,rnad,atiny
+   real(r_kind):: bias,scan,errinv,rnad
    real(r_kind):: tlaptmp,tsumtmp,ratio
    real(r_kind),allocatable,dimension(:):: tsum0,tsum,tlap0,tlap1,tlap2,tcnt
    real(r_kind),allocatable,dimension(:,:):: AA
@@ -1780,6 +1795,7 @@ contains
 !           errinv=data_chan(j)%errinv
 !           if (iuse_rad(jj)<=0) errinv=exp(-(data_chan(j)%omgnbc/3.0_r_kind)**2)
                   errinv=exp(-(data_chan(j)%omgnbc/3.0_r_kind)**2)
+                  if (errinv<atiny) cycle loopc
 
                   if (update_tlapmean(jj)) then
                      tlaptmp=data_chan(j)%tlap
@@ -1880,7 +1896,6 @@ contains
          endif
 
 !        Solve linear system
-         atiny=1.0e-10_r_kind
          allocate(AA(np,np),be(np))
          do i=1,new_chan
             if (iobs(i)<nthreshold) cycle
