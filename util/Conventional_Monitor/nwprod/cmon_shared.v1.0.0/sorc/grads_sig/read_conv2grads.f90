b51f14c744ee7a550abb2766a46a847916ae8e94
edward.safford@noaa.gov <edward.safford@noaa.gov>
2017-04-21 15:40:59 +0000

    Modify grads_sig to eliminate the 2 stage write, replacing it with a linked list.

--- util/Conventional_Monitor/nwprod/cmon_shared.v1.0.0/sorc/grads_sig/read_conv2grads.f90
+++ util/Conventional_Monitor/nwprod/cmon_shared.v1.0.0/sorc/grads_sig/read_conv2grads.f90
@@ -13,45 +13,38 @@
 !        stype   : the observation sub type, like t120 uv220
 !----------------------------------------------------------------------
 
-subroutine read_conv2grads(ctype,stype,intype,target_nreal,nobs,isubtype,subtype)
+subroutine read_conv2grads(ctype,stype,intype,target_nreal,nobs,isubtype,subtype,list)
 
-!       nreal1 is target_nreal 
-!       nreal is file_nreal  (now renamed)
+   use generic_list
+   use data
 
    implicit none
-
-   real(4),allocatable,dimension(:,:)  :: rdiag 
+  
+   type(list_node_t), pointer :: list
+   type(list_node_t), pointer :: next => null()
+   type(data_ptr) :: ptr
+ 
+   real(4),allocatable,dimension(:,:)     :: rdiag 
    character(8),allocatable,dimension(:)  :: cdiag 
 
-   character(3) :: dtype,ctype
-   character(2) :: subtype 
+   character(3)   :: dtype,ctype
+   character(2)   :: subtype 
    character(10)  :: stype,otype
    character(15)  :: fileo,fileo_subtyp
 
    integer nchar,file_nreal,i,ii,mype,idate,iflag,itype,iscater,igrads
-   integer lunin,lunot,lunot2,target_nreal,ldtype,intype,isubtype,jsubtype
-   integer nobs
- 
-   logical lexist
-   data lunin / 11 /
-   data lunot / 21 /
-   data lunot2 / 22 /
+   integer lunin,lunot,target_nreal,ldtype,intype,isubtype,jsubtype
+   integer nobs,idx
 
+   data lunin / 11 /
 
    nobs=0
-   print *,'target_nreal=',target_nreal
-   print *,'intype=',intype
-   
-   fileo=trim(stype)//'_'//trim(subtype)//'.tmp' 
-   print *,fileo
+   print *, '--> read_conv2grads'
 
-   print *,fileo
    open(lunin,file='conv_diag',form='unformatted')  
-   open(lunot,file=fileo,form='unformatted')
    rewind(lunin)
 
    read(lunin) idate
-
    print *, 'idate=',idate 
 
    loopd: do  
@@ -59,22 +52,21 @@ subroutine read_conv2grads(ctype,stype,intype,target_nreal,nobs,isubtype,subtype
       read(lunin,IOSTAT=iflag) dtype,nchar,file_nreal,ii,mype
       if( iflag /= 0 ) exit loopd
 
-      print *, 'observation type:',dtype,' ctype=', ctype
-      print *, 'file_nreal      :',file_nreal,' target_nreal=', target_nreal
-
       if( trim(dtype) == trim(ctype) .and. file_nreal /= target_nreal ) then
          print *, 'observation type:',dtype,' file_nreal=', file_nreal
          exit 
       endif
 
-      allocate(cdiag(ii),rdiag(file_nreal,ii))
-      read(lunin,IOSTAT=iflag) cdiag,rdiag
-      if( iflag /= 0 ) exit loopd
-
       if(trim(dtype) /= trim(ctype))  then
-         deallocate(cdiag,rdiag)
          cycle
       endif
+
+      allocate(cdiag(ii),rdiag(file_nreal,ii))
+      read(lunin,IOSTAT=iflag) cdiag,rdiag
+      if( iflag /= 0 ) then
+         deallocate( cdiag,rdiag )
+         exit loopd
+      end if
 
       do i=1,ii
          itype = int(rdiag(1,i)) 
@@ -82,18 +74,50 @@ subroutine read_conv2grads(ctype,stype,intype,target_nreal,nobs,isubtype,subtype
 
          if(itype == intype .and. jsubtype ==isubtype)  then 
             nobs=nobs+1
-            write(lunot) cdiag(i),rdiag(3:file_nreal,i)
+
+            !---------------------------------------------
+            ! Allocate a new data element and load
+            !
+!            print *, 'Allocating new data element'
+            allocate(ptr%p)
+            ptr%p%stn_id = cdiag(i)
+            do idx=1,max_rdiag_reals
+               if( idx > file_nreal ) then
+                  ptr%p%rdiag( idx ) = 0.00
+               else
+                  ptr%p%rdiag( idx ) = rdiag( idx,i )
+               end if
+            end do 
+
+            if( nobs == 1 ) then
+               !-------------------------------------------------
+               ! Initialize the list with the first data element
+               !
+               call list_init(list, transfer(ptr, list_data))
+!               print *, 'Initializing list with data:', ptr%p
+               next => list
+
+            else
+               !-------------------------------------------------
+               ! Insert subsequent nodes into the list
+               !
+!               print *, 'Inserting node with data:', ptr%p
+               call list_insert(next, transfer(ptr, list_data))
+               next => list_next(next)
+
+            end if
+
          endif
+
       enddo
 
       deallocate(cdiag,rdiag)
    enddo   loopd               !  ending read data do loop
 
-    
    close(lunin)
-   close(lunot)
 
-   print *, 'end of read_conv2grads subroutine'
+   print *, 'nobs added to list = ', nobs
+   print *, '<-- read_conv2grads'
 
    return 
 end 
