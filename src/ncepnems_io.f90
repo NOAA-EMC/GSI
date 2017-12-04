121d1862cae68f8fc794e46a30c24a34a31e9e6f
catherine.thomas <catherine.thomas@noaa.gov>
2017-12-04 12:47:24 -0500

    EXP-gfdlmp: remove read of gfdl_mp.  Assume if ice_wat is available, gfdl_mp=.true.

--- src/ncepnems_io.f90
+++ src/ncepnems_io.f90
@@ -882,13 +882,10 @@ contains
           if (mype==mod(icount-1,npe)) then
              call nemsio_readrecv(gfile,'clwmr','mid layer',k,rwork1d0,iret=iret)
              if (iret /= 0) call error_msg(trim(my_name),trim(filename),'clwmr','read',istop+9,iret)
-             if (gfdl_mp) then
-                call nemsio_readrecv(gfile,'ice_wat','mid layer',k,rwork1d1,iret=iret)
-                if (iret == 0) then
-                   rwork1d0 = rwork1d0 + rwork1d1
-                else
-                   call error_msg(trim(my_name),trim(filename),'ice_wat','read',istop+10,iret)
-                endif
+             call nemsio_readrecv(gfile,'ice_wat','mid layer',k,rwork1d1,iret=iret)
+             if (iret == 0) then
+                gfdl_mp = .true.   
+                rwork1d0 = rwork1d0 + rwork1d1
              endif
              if(diff_res)then
                 grid_b=reshape(rwork1d0,(/size(grid_b,1),size(grid_b,2)/))
