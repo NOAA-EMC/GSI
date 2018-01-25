0fd6f5303a67670f4ceb98892ff2c3d05c263434
Rahul Mahajan <rahul.mahajan@noaa.gov>
2018-01-25 09:51:29 -0500

    EXP-scripts (R.Mahajan) Edit build scripts under ush so they can all build correctly.

--- ush/build_all.sh
+++ ush/build_all.sh
@@ -1,24 +1,16 @@
 #!/bin/sh
 
+set -ex
+
+cd ..
+pwd=$(pwd)
+
 target=$1
+dir_root=${2:-$pwd}
 
 BUILD_GSI=${BUILD_GSI:-"YES"}
 BUILD_ENKF=${BUILD_ENKF:-"YES"}
 BUILD_UTILS=${BUILD_UTILS:-"YES"}
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 wcoss or wcoss_c/cray or theia"
-    exit
-fi
-
-set -x -e
-
-cd ..
-dir_root=$(pwd)
-dir_util=$dir_root/util
-dir_scripts=$dir_root/scripts
-dir_modules=$dir_root/modulefiles
-[ -d $dir_root/exec ] || mkdir -p $dir_root/exec
 
 if [ $target = wcoss ]; then
     . /usrx/local/Modules/3.2.10/init/sh
@@ -34,85 +26,36 @@ else
     exit 9
 fi
 
+dir_modules=$dir_root/modulefiles
+if [ ! -d $dir_modules ]; then
+    echo "modulefiles does not exist in $dir_modules"
+    exit 10
+fi
+[ -d $dir_root/exec ] || mkdir -p $dir_root/exec
+
 # First build GSI
 if [ $BUILD_GSI = "YES" -o $BUILD_ENKF = "YES" ]; then
 
-    module purge
-    if [ $target = wcoss -o $target = cray ]; then
-        module load $dir_modules/modulefile.global_gsi.$target
-    else
-        source $dir_modules/modulefile.global_gsi.$target
-    fi
-    module list
-
-    cd $dir_root/src
-    ./configure clean
-    ./configure $conf_target
-    make -f Makefile clean
-    make -f Makefile -j 8
-    cp -p global_gsi $dir_root/exec
-
-    # EnKF requires GSI modules and objects
-    if [ $BUILD_ENKF = "NO" ]; then
-        make -f Makefile clean
-        ./configure clean
-    fi
+    clean=YES
+    [[ $BUILD_ENKF = "YES" ]] && clean=NO
+    $dir_root/ush/build_gsi.sh $target $pwd $clean
 
 fi
 
 # Next build EnKF
 if [ $BUILD_ENKF = "YES" ]; then
 
-    module purge
-    if [ $target = wcoss -o $target = cray ]; then
-        module load $dir_modules/modulefile.gdas_enkf.$target
-    else
-        source $dir_modules/modulefile.gdas_enkf.$target
-    fi
-    module list
-
-    cd $dir_root/src/enkf
-    ./configure clean
-    ./configure $conf_target
-    make -f Makefile clean
-    make -f Makefile -j 8
-    cp -p global_enkf $dir_root/exec
-    make -f Makefile clean
-    ./configure clean
-    # Now clean the GSI directory
-    cd ..
-    make -f Makefile clean
-    ./configure clean
+    clean=YES
+    $dir_root/ush/build_enkf.sh $target $pwd $clean
 
 fi
 
-# Then build EnKF utilities
+# Next build EnKF utilities
 if [ $BUILD_UTILS = "YES" ]; then
 
-    module purge
-    if [ $target = wcoss -o $target = cray ]; then
-        module load $dir_modules/modulefile.gdas_enkf.$target
-    else
-        source $dir_modules/modulefile.gdas_enkf.$target
-    fi
-    module list
-
-    dlist="adderrspec_nmcmeth_spec.fd getsfcensmeanp.fd getsigensstatp.fd getnstensmeanp.fd getsfcnstensupdp.fd getsigensmeanp_smooth_ncep.fd recentersigp.fd calc_increment_ens.fd gribmean.fd"
-
-    for dir in $dlist; do
-
-        cd $dir_root/util/EnKF/gfs/src/$dir
-        ./configure clean
-        ./configure $conf_target
-        make -f Makefile clean
-        make -f Makefile
-        cp -p *.x $dir_root/exec
-        rm -f $dir_root/exec/log*.x
-        make -f Makefile clean
-        ./configure clean
-
-    done
+    clean=YES
+    $dir_root/ush/build_enkf_utils.sh $target $pwd $clean
 
 fi
 
-exit
+exit 0
