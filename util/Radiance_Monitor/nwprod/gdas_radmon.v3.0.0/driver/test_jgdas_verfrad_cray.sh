354e9083b7efb95da5ec3d1413f2d96abfa20504
edward.safford <edward.safford@noaa.gov>
2018-09-20 15:26:29 +0000

    Update RadMon to add /${cyc} subdirectory to $COM_IN and $TANKverf_rad definitions, per fv3 directory scheme.
    Change-Id: Iefbc6f020cb91d6302616e4d361a274cf1a2a758

--- util/Radiance_Monitor/nwprod/gdas_radmon.v3.0.0/driver/test_jgdas_verfrad_cray.sh
+++ util/Radiance_Monitor/nwprod/gdas_radmon.v3.0.0/driver/test_jgdas_verfrad_cray.sh
@@ -5,15 +5,23 @@
 #BSUB -J gdas_verfrad
 #BSUB -q dev
 #BSUB -M 100
-#BSUB -W 00:20
+#BSUB -W 00:40
 #BSUB -P GFS-T2O
 #BSUB -R "select[mem>100] rusage[mem=100]"
-##BSUB -cwd /gpfs/hps/ptmp/Edward.Safford
+
+##BSUB -cwd /gpfs/hps2/ptmp/Edward.Safford
 ##BSUB -cwd ${PWD}
+=======
+
+
+ulimit -c 0
+ulimit -s unlimited
 
 set -x
 
-export PDATE=${PDATE:-2016100606}
+export MY_MACHINE=cray
+
+export PDATE=${PDATE:-2016100206}
 
 #############################################################
 # Specify whether the run is production or development
@@ -28,10 +36,11 @@ export envir=para
 #############################################################
 # Specify versions
 #############################################################
-export gdas_ver=v14.1.0
-export global_shared_ver=v14.1.0
-export gdas_radmon_ver=v2.0.0
-export radmon_shared_ver=v2.0.4
+export gdas_ver=v15.0.0
+export global_shared_ver=v15.0.0
+
+export gdas_radmon_ver=v3.0.0
+export radmon_shared_ver=v3.0.0
 
 
 #############################################################
@@ -40,9 +49,10 @@ export radmon_shared_ver=v2.0.4
 . $MODULESHOME/init/ksh
 
 module load prod_util
-
 module list
 
+#echo "locating aprun:"
+#which aprun
 
 #############################################################
 # WCOSS environment settings
@@ -53,13 +63,15 @@ export POE=YES
 #############################################################
 # Set user specific variables
 #############################################################
-export DATAROOT=${DATAROOT:-/gpfs/hps/emc/da/noscrub/$LOGNAME/test_data}
-export COMROOT=${COMROOT:-/gpfs/hps/ptmp/$LOGNAME/com}
+export DATAROOT=${DATAROOT:-/gpfs/hps3/emc/da/noscrub/$LOGNAME/test_data}
+export COMROOT=${COMROOT:-/gpfs/hps2/ptmp/$LOGNAME/com}
 export RADMON_SUFFIX=${RADMON_SUFFIX:-testrad}
-export NWTEST=${NWTEST:-/gpfs/hps/emc/da/noscrub/${LOGNAME}/gfs_q3fy17}
-export HOMEgdas=${NWTEST}/gdas.${gdas_ver}
+
+export NWTEST=${NWTEST:-/gpfs/hps3/emc/da/noscrub/${LOGNAME}/ProdGSI/util/Radiance_Monitor/nwprod}
+export HOMEgdas=${NWTEST}/gdas_radmon.${gdas_radmon_ver}
 export JOBGLOBAL=${HOMEgdas}/jobs
-export HOMEradmon=${NWTEST}/global_shared.${global_shared_ver}
+
+export HOMEradmon=${NWTEST}/radmon_shared.${radmon_shared_ver}
 export COM_IN=${DATAROOT}
 export TANKverf=${TANKverf:-${COMROOT}/${RADMON_SUFFIX}}
 
