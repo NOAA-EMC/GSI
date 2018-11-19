06ba6789627cd1f3c9921d8c68b3730f9ede4c1c
edward.safford <edward.safford@noaa.gov>
2018-11-19 18:08:42 +0000

    VLab Issue #55400. Update MinMon with fv3 changes. Fixes #55400.
    Change-Id: Idaa34abded48dc920d6188e83fc7742f921d3f20

---
+++ util/Minimization_Monitor/data_xtrct/ush/run_gfs_DE.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+ch=`hostname | cut -c1`
+
+#package=MinMon_598
+package=ProdGSI/util/Minimization_Monitor
+
+suffix=GFS
+export RUN=gfs
+
+scripts=/gpfs/${ch}d2/emc/da/noscrub/${USER}/${package}/data_xtrct/ush
+export jobfile=/gpfs/${ch}d2/emc/da/noscrub/${USER}/${package}/nwprod/gfs.v1.0.0/jobs/JGFS_VMINMON
+
+. /usrx/local/Modules/3.2.9/init/sh
+module use -a /gpfs/hps/nco/ops/nwprod/modulefiles
+module load prod_util
+
+#NDATE=/nwprod/util/exec/ndate
+
+
+tank=~/nbns/stats/${suffix}
+ldate=`${scripts}/find_cycle.pl ${suffix} --cyc 1 --dir ${tank} --run ${RUN}`
+
+export PDATE=`${NDATE} +06 $ldate`
+#export PDATE=2018102800
+
+export PDY=`echo $PDATE|cut -c1-8`
+export cyc=`echo $PDATE|cut -c9-10`
+
+
+export COMIN=/gpfs/hps/nco/ops/com/gfs/prod/gfs.${PDY}
+export gsistat=${COMIN}/gfs.t${cyc}z.gsistat
+
+tank=~/nbns/stats/${suffix}
+logdir=/ptmpd1/Edward.Safford/logs/${suffix}/${RUN}/minmon
+
+echo PDATE   = $PDATE
+echo gsistat = $gsistat
+
+#${scripts}/MinMon_DE.sh ${suffix} ${PDATE}
+${scripts}/MinMon_DE.sh ${suffix} --pdate ${PDATE} --run ${RUN} 1>$logdir/MinMon_DE.log 2>$logdir/MinMon_DE.err
