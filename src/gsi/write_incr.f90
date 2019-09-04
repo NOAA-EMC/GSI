51b8c42c89d0693a685d97dd4ee298a41ce69a7c
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2019-09-04 21:00:39 +0000

    Have working output of T/q increment

--- src/gsi/write_incr.f90
+++ src/gsi/write_incr.f90
@@ -25,7 +25,7 @@ module write_incr
 
 contains
 
-  subroutine write_fv3_inc_ (grd,sp_a,filename,mype_out,gfs_bundle,ibin)
+  subroutine write_fv3_inc_ (grd,sp_a,filename,mype_out,gfs_bundle,sval,ibin)
 
 !$$$  subprogram documentation block
 !                .      .    .
@@ -62,7 +62,7 @@ contains
     use mpimod, only: ierror
     use mpimod, only: mype
 
-    use gridmod, only: strip
+    use gridmod, only: strip, rlats, rlons
 
     use general_commvars_mod, only: load_grid
     use general_specmod, only: spec_vars
@@ -70,6 +70,10 @@ contains
 
     use gsi_bundlemod, only: gsi_bundle, gsi_bundlegetpointer
     use control_vectors, only: lupp
+
+    use constants, only: one, fv, rad2deg
+
+    use gsi_4dvar, only: nobs_bins
 
     implicit none
 
@@ -80,15 +84,16 @@ contains
     character(len=24),   intent(in) :: filename  ! file to open and write to
     integer(i_kind),     intent(in) :: mype_out  ! mpi task to write output file
     type(gsi_bundle),    intent(in) :: gfs_bundle
+    type(gsi_bundle),    intent(in) :: sval(nobs_bins)
     integer(i_kind),     intent(in) :: ibin      ! time bin
 
 !-------------------------------------------------------------------------
 
     character(len=120) :: my_name = 'WRITE_FV3INCR'
 
-    real(r_kind),pointer,dimension(:,:) :: sub_ps
-    real(r_kind),pointer,dimension(:,:,:) :: sub_u,sub_v,sub_tv
-    real(r_kind),pointer,dimension(:,:,:) :: sub_q,sub_oz,sub_cwmr
+    !real(r_kind),pointer,dimension(:,:) :: sub_ps
+    real(r_kind),pointer,dimension(:,:,:) :: sub_u,sub_v,sub_tsen
+    real(r_kind),pointer,dimension(:,:,:) :: sub_q,sub_qana,sub_oz,sub_cwmr
 
     real(r_kind),dimension(grd%lat2,grd%lon2,grd%nsig) :: sub_dzb,sub_dza
     real(r_kind),dimension(grd%lat2,grd%lon2,grd%nsig) :: sub_prsl
@@ -97,13 +102,16 @@ contains
 
     real(r_kind),dimension(grd%lat1*grd%lon1)     :: psm
     real(r_kind),dimension(grd%lat2,grd%lon2,grd%nsig):: sub_dp
-    real(r_kind),dimension(grd%lat1*grd%lon1,grd%nsig):: tvsm,prslm, usm, vsm
-    real(r_kind),dimension(grd%lat1*grd%lon1,grd%nsig):: dpsm, qsm, ozsm
+    real(r_kind),dimension(grd%lat1*grd%lon1,grd%nsig):: tsensm,prslm, usm, vsm
+    real(r_kind),dimension(grd%lat1*grd%lon1,grd%nsig):: dpsm, qsm, qsmana, ozsm
     real(r_kind),dimension(grd%lat1*grd%lon1,grd%nsig):: cwsm, dzsm
     real(r_kind),dimension(max(grd%iglobal,grd%itotsub)) :: work1,work2
-    real(r_kind),dimension(grd%nlon,grd%nlat-2):: grid
+    real(r_kind),dimension(grd%nlon,grd%nlat-2):: grid, gridrev
+    real(r_kind),dimension(grd%nlon) :: deglons
+    real(r_kind),dimension(grd%nlat-2) :: deglats
+    real(r_kind),dimension(grd%nsig) :: levsout
 
-    integer(i_kind) :: mm1, k
+    integer(i_kind) :: mm1, i, j, k
     integer(i_kind) :: iret, istatus 
     integer(i_kind) :: ncid_out, lon_dimid, lat_dimid, lev_dimid, ilev_dimid
     integer(i_kind) :: lonvarid, latvarid, levvarid, pfullvarid, ilevvarid, &
@@ -116,13 +124,14 @@ contains
     mm1=mype+1
 
     istatus=0
-    call gsi_bundlegetpointer(gfs_bundle,'ps', sub_ps,  iret); istatus=istatus+iret
-    call gsi_bundlegetpointer(gfs_bundle,'u',  sub_u,   iret); istatus=istatus+iret
-    call gsi_bundlegetpointer(gfs_bundle,'v',  sub_v,   iret); istatus=istatus+iret
-    call gsi_bundlegetpointer(gfs_bundle,'tv', sub_tv,  iret); istatus=istatus+iret
-    call gsi_bundlegetpointer(gfs_bundle,'q',  sub_q,   iret); istatus=istatus+iret
-    call gsi_bundlegetpointer(gfs_bundle,'oz', sub_oz,  iret); istatus=istatus+iret
-    call gsi_bundlegetpointer(gfs_bundle,'cw', sub_cwmr,iret); istatus=istatus+iret
+    !call gsi_bundlegetpointer(gfs_bundle,'u',  sub_u,   iret); istatus=istatus+iret
+    !call gsi_bundlegetpointer(gfs_bundle,'v',  sub_v,   iret); istatus=istatus+iret
+    ! TODO CRM - what is the correct index for sval? always 1? related to nfldsig?
+    call gsi_bundlegetpointer(sval(1),'tsen', sub_tsen,  iret); istatus=istatus+iret
+    !call gsi_bundlegetpointer(sval(1),'tv', sub_tv,  iret); istatus=istatus+iret
+    call gsi_bundlegetpointer(gfs_bundle,'q',  sub_qana,   iret); istatus=istatus+iret ! need this one for Tv to Tsen
+    call gsi_bundlegetpointer(sval(1),'q',  sub_q,   iret); istatus=istatus+iret
+    call gsi_bundlegetpointer(sval(1),'oz', sub_oz,  iret); istatus=istatus+iret
     if ( istatus /= 0 ) then
        if ( mype == 0 ) then
          write(6,*) 'write_fv3_incr_: ERROR'
@@ -135,16 +144,12 @@ contains
     ! Single task writes increment to file
     if ( mype == mype_out ) then
       ! create the output netCDF file
-      call nccheck_incr(nf90_create(path=trim(filename), cmode=ior(nf90_clobber,nf90_64bit_offset), ncid=ncid_out))
+      call nccheck_incr(nf90_create(path=trim(filename)//".nc", cmode=ior(nf90_clobber,nf90_64bit_offset), ncid=ncid_out))
       ! create dimensions based on analysis resolution, not guess
       call nccheck_incr(nf90_def_dim(ncid_out, "lon", grd%nlon, lon_dimid))
-      call nccheck_incr(nf90_def_dim(ncid_out, "lat", grd%nlat, lat_dimid))
+      call nccheck_incr(nf90_def_dim(ncid_out, "lat", grd%nlat-2, lat_dimid))
       call nccheck_incr(nf90_def_dim(ncid_out, "lev", grd%nsig, lev_dimid))
       call nccheck_incr(nf90_def_dim(ncid_out, "ilev", grd%nsig+1, ilev_dimid))
-      ! place global attributes to parallel calc_increment output
-      call nccheck_incr(nf90_put_att(ncid_out, nf90_global, "source", "GSI"))
-      call nccheck_incr(nf90_put_att(ncid_out, nf90_global, "comment", &
-                                      "global analysis increment from write_fv3_increment"))
       dimids3 = (/ lon_dimid, lat_dimid, lev_dimid /)
       ! create variables
       call nccheck_incr(nf90_def_var(ncid_out, "lon", nf90_real, (/lon_dimid/), lonvarid))
@@ -163,22 +168,99 @@ contains
       call nccheck_incr(nf90_def_var(ncid_out, "liq_wat_inc", nf90_real, dimids3, liqwatvarid)) 
       call nccheck_incr(nf90_def_var(ncid_out, "o3mr_inc", nf90_real, dimids3, o3varid)) 
       call nccheck_incr(nf90_def_var(ncid_out, "icmr_inc", nf90_real, dimids3, icvarid)) 
+      ! place global attributes to parallel calc_increment output
+      call nccheck_incr(nf90_put_att(ncid_out, nf90_global, "source", "GSI"))
+      call nccheck_incr(nf90_put_att(ncid_out, nf90_global, "comment", &
+                                      "global analysis increment from write_fv3_increment"))
+      ! add units to lat/lon because that's what the calc_increment utility has
+      call nccheck_incr(nf90_put_att(ncid_out, lonvarid, "units", "degrees_east"))
+      call nccheck_incr(nf90_put_att(ncid_out, latvarid, "units", "degrees_north"))
+      ! end the netCDF file definition
       call nccheck_incr(nf90_enddef(ncid_out))
     end if
 
     ! Strip off boundary points from subdomains
-    call strip(sub_ps  ,psm)
-    call strip(sub_tv  ,tvsm  ,grd%nsig)
+    call strip(sub_tsen  ,tsensm  ,grd%nsig)
     call strip(sub_q   ,qsm   ,grd%nsig)
+    call strip(sub_qana   ,qsmana  ,grd%nsig)
     call strip(sub_oz  ,ozsm  ,grd%nsig)
-    call strip(sub_cwmr,cwsm  ,grd%nsig)
-    call strip(sub_dp  ,dpsm  ,grd%nsig)
-    call strip(sub_prsl,prslm ,grd%nsig)
-    call strip(sub_u   ,usm   ,grd%nsig)
-    call strip(sub_v   ,vsm   ,grd%nsig)
-    if (lupp) call strip(sub_dza ,dzsm  ,grd%nsig)
+    !call strip(sub_cwmr,cwsm  ,grd%nsig)
+    !call strip(sub_dp  ,dpsm  ,grd%nsig)
+    !call strip(sub_prsl,prslm ,grd%nsig)
+    !call strip(sub_u   ,usm   ,grd%nsig)
+    !call strip(sub_v   ,vsm   ,grd%nsig)
+    !if (lupp) call strip(sub_dza ,dzsm  ,grd%nsig)
 
-    nccount = (/ grd%nlon, grd%nlat, 1 /)
+    nccount = (/ grd%nlon, grd%nlat-2, 1 /)
+    if (mype == mype_out) then
+       ! latitudes
+       do j=1,grd%nlat-2
+          deglats(j) = rlats(j)*rad2deg
+       end do
+       ! write to file
+       call nccheck_incr(nf90_put_var(ncid_out, latvarid, sngl(deglats), &
+                         start = (/1/), count = (/grd%nlat-2/)))
+       ! longitudes
+       do i=1,grd%nlon
+          deglons(i) = rlons(i)*rad2deg
+       end do
+       ! write to file
+       call nccheck_incr(nf90_put_var(ncid_out, lonvarid, sngl(deglons), &
+                         start = (/1/), count = (/grd%nlon/)))
+       ! levels
+       do k=1,grd%nsig
+         levsout(k) = float(k)
+       end do
+       ! write to file
+       call nccheck_incr(nf90_put_var(ncid_out, levvarid, sngl(levsout), &
+                         start = (/1/), count = (/grd%nsig/)))
+       ! pfull
+       ! ilev
+       ! hyai
+       ! hybi
+    end if
+    ! u and v increment
+    ! delp increment
+    ! delz increment
+    ! Temperature Increment
+    ! Convert Tv to T
+    !tvsm = tvsm/(one+fv*qsmana)
+    ncstart = (/ 1, 1, grd%nsig /)
+    do k=1,grd%nsig
+       call mpi_gatherv(tsensm(1,k),grd%ijn(mm1),mpi_rtype,&
+            work1,grd%ijn,grd%displs_g,mpi_rtype,&
+            mype_out,mpi_comm_world,ierror)
+       if (mype == mype_out) then
+          call load_grid(work1,gridrev)
+          ! GSI is N->S, we want S->N 
+          do j=1,grd%nlat-2
+             grid(:,j) = gridrev(:,grd%nlat-1-j)
+          end do
+          ! write to file
+          call nccheck_incr(nf90_put_var(ncid_out, tvarid, sngl(grid), &
+                            start = ncstart, count = nccount))
+       endif
+       ncstart(3) = grd%nsig-k ! GSI is sfc->top, FV3 is top->sfc
+    end do
+    ! specific humidity increment
+    ncstart = (/ 1, 1, grd%nsig /)
+    do k=1,grd%nsig
+       call mpi_gatherv(qsm(1,k),grd%ijn(mm1),mpi_rtype,&
+            work1,grd%ijn,grd%displs_g,mpi_rtype,&
+            mype_out,mpi_comm_world,ierror)
+       if (mype == mype_out) then
+          call load_grid(work1,gridrev)
+          ! GSI is N->S, we want S->N 
+          do j=1,grd%nlat-2
+             grid(:,j) = gridrev(:,grd%nlat-1-j)
+          end do
+          ! write to file
+          call nccheck_incr(nf90_put_var(ncid_out, sphumvarid, sngl(grid), &
+                            start = ncstart, count = nccount))
+       endif
+       ncstart(3) = grd%nsig-k ! GSI is sfc->top, FV3 is top->sfc
+    end do
+    ! liquid water increment
     ! ozone increment
     ncstart = (/ 1, 1, grd%nsig /)
     do k=1,grd%nsig
@@ -186,18 +268,23 @@ contains
             work1,grd%ijn,grd%displs_g,mpi_rtype,&
             mype_out,mpi_comm_world,ierror)
        if (mype == mype_out) then
-          call load_grid(work1,grid)
+          call load_grid(work1,gridrev)
+          ! GSI is N->S, we want S->N 
+          do j=1,grd%nlat-2
+             grid(:,j) = gridrev(:,grd%nlat-1-j)
+          end do
           ! write to file
-          call nccheck_incr(nf90_put_var(ncid_out, o3varid, grid, &
+          call nccheck_incr(nf90_put_var(ncid_out, o3varid, sngl(grid), &
                             start = ncstart, count = nccount))
        endif
-       ncstart(3) = grd%nsig-k
+       ncstart(3) = grd%nsig-k ! GSI is sfc->top, FV3 is top->sfc
     end do
+    ! ice mixing ratio increment
 
    ! cleanup and exit
    if ( mype == mype_out ) then
       call nccheck_incr(nf90_close(ncid_out))
-      write(6,*) "FV3 netCDF increment written, file=",filename
+      write(6,*) "FV3 netCDF increment written, file= "//trim(filename)//".nc"
    end if
 
 
