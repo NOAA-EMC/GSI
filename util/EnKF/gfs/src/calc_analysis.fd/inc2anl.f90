5d36c8c3ee72af28fd19a3fa78f8c630c7f34004
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2019-09-18 12:31:17 +0000

    Issue #68591 - commit to switch branch

--- util/EnKF/gfs/src/calc_analysis.fd/inc2anl.f90
+++ util/EnKF/gfs/src/calc_analysis.fd/inc2anl.f90
@@ -50,17 +50,17 @@ contains
         end if
       end do
       if (use_increment) then
-         if (trim(recname(i)) == 'pres') then
-           ! special case for surface pressure
-           print *, 'pres'
-           ! placeholder
-         else
-           ! call generic subroutine for all other fields
-           call add_increment(recname(i), reclevtyp(i), reclev(i), incvars_netcdf(iincvar))
-           print *, recname(i), reclevtyp(i), reclev(i), incvars_netcdf(iincvar)
-         end if
+        print *, 'Adding Increment', recname(i), reclevtyp(i), reclev(i), incvars_netcdf(iincvar)
+        if (trim(recname(i)) == 'pres') then
+          ! special case for surface pressure
+          call add_psfc_increment
+        else
+          ! call generic subroutine for all other fields
+          call add_increment(recname(i), reclevtyp(i), reclev(i), incvars_netcdf(iincvar))
+        end if
       else
         ! otherwise just write out what is in the input to the output
+        print *, 'Copying from Background', recname(i), reclevtyp(i), reclev(i), incvars_netcdf(iincvar)
         call nemsio_readrecv(fcstfile, recname(i), reclevtyp(i), reclev(i), work1, iret=iret)
         if (iret /=0) write(6,*) 'Error with NEMSIO read', recname(i),reclevtyp(i), reclev(i)
         call nemsio_writerecv(anlfile, recname(i), reclevtyp(i), reclev(i), work1, iret=iret)
@@ -70,6 +70,17 @@ contains
   end subroutine compute_anl
 
   subroutine add_increment(recname, reclevtyp, reclev, incvar_nc)
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  ! subroutine add_increment
+  !            generic subroutine for adding increment to background
+  !            and writing out to analysis 
+  !  args:
+  !       recname   - input string of NEMSIO record name
+  !       reclevtyp - input string of NEMSIO record level type
+  !       reclev    - input integer of NEMSIO record level
+  !       incvar_nc - input string of name of variable in netCDF file
+  !                   (without _inc suffix added)
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     use vars_calc_analysis, only: fcstfile, anlfile, nlat, nlon, nlev,&
                                   incr_file
                                   
@@ -114,9 +125,66 @@ contains
     ! write out analysis for this field
     work1 = reshape(work2d_bg,(/size(work1)/))
     call nemsio_writerecv(anlfile, recname, reclevtyp, reclev, work1, iret=iret)
-    if (iret /=0) write(6,*) 'Error with NEMSIO write', recname,reclevtyp, reclev
+    if (iret /=0) write(6,*) 'Error with NEMSIO write', recname, reclevtyp, reclev
 
   end subroutine add_increment
+
+  subroutine add_psfc_increment
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  ! subroutine add_psfc_increment
+  !            special case of getting surface pressure analysis from
+  !            the integral of the sum of the delp increment and delp background 
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    use vars_calc_analysis, only: fcstfile, anlfile, nlat, nlon, nlev, &
+                                  incr_file
+                                  
+    use nemsio_module
+    use netcdf
+    implicit none
+
+    ! local variables
+    real, allocatable, dimension(:) :: work1
+    real, allocatable, dimension(:,:) :: work2d_delp, psfc
+    real, allocatable, dimension(:,:,:) :: work3d_inc, work3d_delp
+    integer :: ilev,j,jj,iret,k
+    integer :: ncid, varid
+    integer :: start(3), count(3)
+
+    ! loop through all levels and save the background delp in memory
+    allocate(work1(nlat*nlon))
+    allocate(work2d_delp(nlon,nlat),work3d_delp(nlev,nlon,nlat))
+   
+    do k=1,nlev 
+      call nemsio_readrecv(fcstfile, 'dpres', 'mid layer', k, work1, iret=iret)
+      if (iret /=0) write(6,*) 'Error with NEMSIO read dpres lev=',k
+      work2d_delp = reshape(work1,(/nlon,nlat/))
+      work3d_delp(k,:,:) = work2d_delp
+    end do
+
+    ! read in the netCDF increment for delp
+    allocate(work3d_inc(nlev,nlon,nlat))
+    do k=1,nlev
+      ! get the level from the netCDF file (opposite of NEMSIO)
+      ilev = (nlev+1)-reclev ! ilev = 64 when nlev=64 and reclev=1
+      start = (/1, 1, ilev/)
+      count = (/nlon, nlat, 1/)
+      call nccheck(nf90_open(trim(incr_file), nf90_nowrite, ncid)) 
+      call nccheck(nf90_inq_varid(ncid, "delp_inc", varid))
+      call nccheck(nf90_get_var(ncid, varid, work2d_nc, start=start, count=count))
+      call nccheck(nf90_close(ncid))
+
+
+    ! compute psfc
+    allocate(psfc(nlon,nlat))
+
+    ! read in the first guess for this field/level
+    allocate(work1(nlat*nlon))
+    allocate(work2d_bg(nlon,nlat))
+    call nemsio_readrecv(fcstfile, 'pres', 'sfc', 1, work1, iret=iret)
+    if (iret /=0) write(6,*) 'Error with NEMSIO read surface pressure'
+    work2d_bg = reshape(work1,(/nlon,nlat/))
+      
+  end subroutine add_psfc_increment
 
   subroutine nccheck(status)
     use netcdf
