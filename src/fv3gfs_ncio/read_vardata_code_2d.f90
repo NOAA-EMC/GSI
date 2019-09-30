9c7e60e019314fcdae5dfa2323d31757bf4b7fb3
jswhit <Jeffrey.S.Whitaker@noaa.gov>
2019-09-30 16:06:19 +0000

    build with included fv3gfs_ncio source

---
+++ src/fv3gfs_ncio/read_vardata_code_2d.f90
@@ -0,0 +1,44 @@
+    type(Dataset), intent(in) :: dset
+    character(len=*), intent(in) :: varname
+    integer, intent(in), optional :: nslice
+    integer, intent(out), optional :: errcode
+    integer ncerr, nvar, n1,n2,n3, ncount
+    logical return_errcode
+    if(present(errcode)) then
+       return_errcode=.true.
+       errcode = 0
+    else
+       return_errcode=.false.
+    endif
+    if (present(nslice)) then
+       ncount = nslice
+    else
+       ncount = 1
+    endif
+    nvar = get_nvar(dset,varname)
+    if (dset%variables(nvar)%ndims /= 2 .and. dset%variables(nvar)%ndims /= 3) then
+       if (return_errcode) then
+          errcode=nf90_ebaddim
+          return
+       else
+          print *,'rank of data array != variable ndims (or ndims-1)'
+          stop "stopped"
+       endif
+    endif
+    n1 = dset%variables(nvar)%dimlens(1)
+    n2 = dset%variables(nvar)%dimlens(2)
+    if (dset%variables(nvar)%ndims == 3) n3 = dset%variables(nvar)%dimlens(4)
+    if (allocated(values)) deallocate(values)
+    allocate(values(n1,n2))
+    if (dset%variables(nvar)%ndims == 3 .and. n3 == 1) then
+       ! return slice along last dimension
+       ncerr = nf90_get_var(dset%ncid, dset%variables(nvar)%varid, values,&
+               start=(/1,1,ncount/), count=(/n1,n2,1/))
+    else
+       ncerr = nf90_get_var(dset%ncid, dset%variables(nvar)%varid, values)
+    endif
+    if (return_errcode) then
+       errcode=ncerr
+    else
+       call nccheck(ncerr)
+    endif
---
+++ src/fv3gfs_ncio/write_vardata_code.f90
@@ -0,0 +1,67 @@
+    type(Dataset), intent(inout) :: dset
+    character(len=*), intent(in) :: varname
+    integer, intent(in), optional :: nslice
+    integer, intent(out), optional :: errcode
+    integer ncerr, nvar, ncount, n1,n2,n3
+    logical is_slice
+    logical return_errcode
+    if(present(errcode)) then
+       return_errcode=.true.
+       errcode = 0
+    else
+       return_errcode=.false.
+    endif
+    if (present(nslice)) then
+       ncount = nslice
+       is_slice = .true.
+    else
+       is_slice = .false.
+    endif
+    nvar = get_nvar(dset,varname)
+    if (is_slice) then
+        if (dset%variables(nvar)%ndims == 4) then
+           n1 = dset%variables(nvar)%dimlens(1)
+           n2 = dset%variables(nvar)%dimlens(2)
+           n3 = dset%variables(nvar)%dimlens(3)
+           ncerr = nf90_put_var(dset%ncid, dset%variables(nvar)%varid,values, &
+                   start=(/1,1,1,ncount/),count=(/n1,n2,n3,1/))
+        else if (dset%variables(nvar)%ndims == 3) then
+           n1 = dset%variables(nvar)%dimlens(1)
+           n2 = dset%variables(nvar)%dimlens(2)
+           ncerr = nf90_put_var(dset%ncid, dset%variables(nvar)%varid,values, &
+                   start=(/1,1,ncount/),count=(/n1,n2,1/))
+        else if (dset%variables(nvar)%ndims == 2) then
+           n1 = dset%variables(nvar)%dimlens(1)
+           ncerr = nf90_put_var(dset%ncid, dset%variables(nvar)%varid,values, &
+                   start=(/1,ncount/),count=(/n1,1/))
+        else if (dset%variables(nvar)%ndims == 1) then
+           if (return_errcode) then
+              errcode = -1
+           else
+              print *,'cannot write a slice to a 1d variable'
+              stop "stopped"
+           endif
+        else if (dset%variables(nvar)%ndims > 4) then
+           if (return_errcode) then
+              errcode = -1
+           else
+              print *,'only variables up to 4d supported'
+              stop "stopped"
+           endif
+        endif
+    else
+        ncerr = nf90_put_var(dset%ncid, dset%variables(nvar)%varid, values)
+    endif
+    if (return_errcode) then
+       errcode=ncerr
+    else
+       call nccheck(ncerr)
+    endif
+    ! reset unlim dim size for all variables
+    if (dset%variables(nvar)%hasunlim) then
+        if (return_errcode) then
+           call set_varunlimdimlens_(dset,errcode)
+        else
+           call set_varunlimdimlens_(dset)
+        endif
+    endif 
