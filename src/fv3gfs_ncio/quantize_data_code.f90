8d99ba4374ec2ba852ac494c1d8628b22524ad58
jswhit2 <Jeffrey.S.Whitaker@noaa.gov>
2019-10-18 17:08:30 +0000

    add has_attr/has_var functions

--- src/fv3gfs_ncio/quantize_data_code.f90
+++ src/fv3gfs_ncio/quantize_data_code.f90
@@ -1,6 +1,12 @@
    integer, intent(in) :: nbits
    real(4), intent(out) :: compress_err
    real(4) dataMin, dataMax, scale_fact, offset
+   ! if nbits not between 1 and 31, don't do anything
+   if (nbits <= 0 .or. nbits > 31) then
+      dataOut = dataIn
+      compress_err = 0.0
+      return
+   endif
    dataMax = maxval(dataIn); dataMin = minval(dataIn)
    ! convert data to 32 bit integers in range 0 to 2**nbits-1, then cast
    ! cast back to 32 bit floats (data is then quantized in steps
