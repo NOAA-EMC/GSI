a4a97256a77cc1c1725ea8404ac68166a675a3e4
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2019-10-24 16:13:10 +0000

    Issue #68591 - make calc_analysis support netCDF guess and either netCDF or NEMSIO analysis

--- util/netcdf_io/calc_analysis.fd/vars_calc_analysis.f90
+++ util/netcdf_io/calc_analysis.fd/vars_calc_analysis.f90
@@ -4,6 +4,7 @@
 !!        for the calc_analysis utility
 !! Original: 2019-09-18   martin   - original module
 !!           2019-09-26   martin   - add support for netCDF read/write
+!!           2019-10-24   martin   - support NEMSIO output write
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 module vars_calc_analysis
   use nemsio_module, only: nemsio_gfile
@@ -14,28 +15,26 @@ module vars_calc_analysis
   public :: anal_file, fcst_file, incr_file
   public :: idate, jdate
   public :: idate6, jdate6
-  public :: nrec, nfday, nfhour, nfminute, nfsecondn, nfsecondd
+  public :: nfday, nfhour, nfminute, nfsecondn, nfsecondd
   public :: nlon, nlat, nlev, nframe, nsoil, ntrac
-  public :: lats, lons
-  public :: recname, reclevtyp, reclev
-  public :: fcstfile, anlfile
+  public :: lats, lons, vcoord
+  public :: anlfile
   public :: work1
   public :: nhr_assim
-  public :: use_nemsio
+  public :: use_nemsio_anl
   public :: fcstncfile, anlncfile, incncfile
 
   character(len=500) :: anal_file, fcst_file, incr_file
   integer, dimension(7) :: idate, jdate
   integer, dimension(6) :: idate6, jdate6
-  integer :: nrec, nfday, nfhour, nfminute, nfsecondn, nfsecondd
+  integer :: nfday, nfhour, nfminute, nfsecondn, nfsecondd
   integer :: nlon, nlat, nlev, nframe, nsoil, ntrac
   real, allocatable, dimension(:) :: lats, lons
-  character(10), allocatable, dimension(:) :: recname, reclevtyp
-  integer, allocatable, dimension(:) :: reclev
-  type(nemsio_gfile) :: fcstfile, anlfile
+  real, allocatable, dimension(:,:,:) :: vcoord
+  type(nemsio_gfile) :: anlfile
   real, allocatable, dimension(:) :: work1
   integer :: nhr_assim
-  logical :: use_nemsio
+  logical :: use_nemsio_anl
   type(Dataset) :: fcstncfile, anlncfile, incncfile
 
 end module vars_calc_analysis
