c1d037e86aef540fbd4b05abeb6a1f91ad80568a
Edward.Safford <edward.safford@noaa.gov>
2019-11-01 16:26:43 +0000

    VLab issue #67898.  Add fix to gnorm_data.txt problem, port to hera.  Fixes #67898.
    Change-Id: I479723e12494b49d577211e7ad64e033d5757390

--- util/Minimization_Monitor/data_xtrct/ush/run_script.sh
+++ util/Minimization_Monitor/data_xtrct/ush/run_script.sh
@@ -1,52 +1,71 @@
-#!/bin/sh
+#!/bin/ksh
 
-idev=`cat /etc/dev | cut -c1`
-iprod=`cat /etc/prod | cut -c1`
+#----------------------------------------------------------------------
+#
+#  This is a generic data extraction leader script.
+#
+#  The MinMon_DE.sh script may be called directly, or this script,
+#    which calls MinMon_DE.sh can be used to overwrite specific
+#    values in MinMon_DE.sh and/or parm files.
+#
+#  Set MINMON_SUFFIX, RUN to match your data source and run 
+#    value.
+#
+#  COMIN is the location of the gsistat file repository, which
+#    contains the specific $RUN.$PDATE subdirectories.
+#
+#  Use pdate to run a specific cycle.  If commented out then the
+#    M_TANKverf directory will be examined and the pdate will
+#    be the last cycle run + 6 hrs. 
+#    
+#----------------------------------------------------------------------
 
-package=
-suffix=
-echo "user = $USER"
+MINMON_SUFFIX=testmm
+RUN=gfs
+COMIN=/scratch1/NCEPDEV/da/Edward.Safford/noscrub/test_data
 
-export KEEPDATA=NO
-export DO_ERROR_RPT=1
-export MAIL_TO=""
-export MAIL_CC=""
+#pdate=2016030700
 
-scripts=/gpfs/hps3/emc/da/noscrub/${LOGNAME}/${package}/util/Minimization_Monitor/data_xtrct/ush
-echo "scripts = $scripts"
 
-export jobfile=/gpfs/hps3/emc/da/noscrub/${LOGNAME}/${package}/util/Minimization_Monitor/nwprod/gdas.v1.0.0/jobs/JGDAS_VMINMON
-echo "jobfile = $jobfile"
+#----------------------------------------------------------------------
+#  No changes should be necessary below this point.
+#----------------------------------------------------------------------
 
-export MODULESHOME=/opt/modules/3.2.6.7
+. ../../parm/MinMon.ver
+. ../../parm/MinMon_config
 
-. $MODULESHOME/init/sh
-
-module use -a /gpfs/hps/nco/ops/nwprod/modulefiles
-
-module load prod_util
+if [[ $RUN == "gfs" ]]; then
+   export jobfile=${HOMEgfs}/jobs/JGFS_VMINMON
+else
+   export jobfile=${HOMEgdas}/jobs/JGDAS_VMINMON
+fi
 
 echo NDATE = $NDATE
 
-export COMIN=/gpfs/hps3/emc/global/noscrub/emc.glopara/archive/prfv3rt1
+echo "LOGdir = ${LOGdir}"
+if [[ ! -e ${LOGdir} ]]; then
+   mkdir -p ${LOGdir}
+fi
 
-tank=~/nbns/stats/${suffix}
-logdir=/gpfs/hps2/ptmp/Edward.Safford/logs/${suffix}/minmon
+if [[ $pdate == "" ]]; then
+   ldate=`${M_DE_SCRIPTS}/find_cycle.pl --dir ${M_TANKverf}/stats/${MINMON_SUFFIX} --cyc 1 --run ${RUN}`
+   pdate=`${NDATE} +06 $ldate`
+fi
 
-ldate=`${scripts}/find_cycle.pl ${suffix} 1 ${tank}`
-export PDATE=`${NDATE} +06 $ldate`
+pdy=`echo $pdate|cut -c1-8`
+cyc=`echo $pdate|cut -c9-10`
 
+export gsistat=${COMIN}/${RUN}.${pdy}/${cyc}/${RUN}.t${cyc}z.gsistat
 
-export PDY=`echo $PDATE|cut -c1-8`
-export cyc=`echo $PDATE|cut -c9-10`
+if [[ ! -e $gsistat ]]; then
+   echo " unable to locate $gsistat"
+fi
 
-module unload prod_util
-
-export gsistat=${COMIN}/gsistat.gdas.${PDATE}
-
-echo PDATE   = $PDATE
+echo pdate   = $pdate
 echo gsistat = $gsistat
 
-${scripts}/MinMon_DE.sh ${suffix} ${PDATE} 1>$logdir/MinMon_DE.log 2>$logdir/MinMon_DE.err
+
+${M_DE_SCRIPTS}/MinMon_DE.sh ${MINMON_SUFFIX} -p ${pdate} -r ${RUN} \
+	1>$LOGdir/MinMon_DE.log 2>$LOGdir/MinMon_DE.err
 
 
