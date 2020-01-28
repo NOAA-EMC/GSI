fed49fb679bca28c764b1ff8f28d49fa43b22434
Cory.R.Martin@noaa.gov <cory.r.martin@noaa.gov>
2020-01-28 22:05:43 +0000

    Fixed typo/error on nc-start from 2 to 1 for lowest latitudes

--- src/gsi/write_incr.f90
+++ src/gsi/write_incr.f90
@@ -327,7 +327,7 @@ contains
     ncstart = (/ jstart(mype+1), istart(mype+1), 1 /)
     nccount = (/ grd%lon1, grd%lat1, grd%nsig /)
     if (istart(mype+1) == 1) then
-      ncstart = (/ jstart(mype+1), 2, 1 /)
+      ncstart = (/ jstart(mype+1), 1, 1 /)
       nccount = (/ grd%lon1, grd%lat1-1, grd%nsig /)
       j1 = 2
       j2 = grd%lat1-1
