11c2d77920b8b0a1ae02b02f1190fa2761e74a54
edward.safford <edward.safford@noaa.gov>
2020-01-28 13:29:09 +0000

    Ref #70964
    Add cray driver changes.
    
    Change-Id: I51ac4a7bf54a2ffc1f228279aa731c90b1dab0f1

--- util/Conventional_Monitor/nwprod/gdas_conmon.v1.0.0/driver/test_jgdas_vcmon_cray.sh
+++ util/Conventional_Monitor/nwprod/gdas_conmon.v1.0.0/driver/test_jgdas_vcmon_cray.sh
@@ -1,37 +1,27 @@
 #!/bin/ksh
 
-#BSUB -o gdas_vcmon.o%J
-#BSUB -e gdas_vcmon.o%J
-#BSUB -J gdas_vcmon
+#BSUB -o gdas_vconmon.o%J
+#BSUB -e gdas_vconmon.o%J
+#BSUB -J gdas_vconmon
 #BSUB -q dev
-#BSUB -M 80
-#BSUB -W 00:40
+#BSUB -n 1
+#BSUB -M 160
+#BSUB -W 00:50
 #BSUB -P GFS-T2O
-#BSUB -R "select[mem>80] rusage[mem=80]"
+#BSUB -R "select[mem>160] rusage[mem=160]"
 
 set -x
 
-export PDATE=${PDATE:-2017030606}
-
-#############################################################
-# Specify whether the run is production or development
-#############################################################
-export PDY=`echo $PDATE | cut -c1-8`
-export CYC=`echo $PDATE | cut -c9-10`
-export cmon_job=${cmon_job:-gdas_vcmon.${CYC}}
-export pid=${pid:-$$}
-export jobid=${cmon_job}.${pid}
-export envir=${envir:-para}
+export PDATE=${PDATE:-2020012018
 
 
 #############################################################
 # Specify versions
 #############################################################
-export gdas_ver=v13.0.0
-export global_shared_ver=v13.0.0
-export gdas_cmon_ver=v1.0.0
-export cmon_shared_ver=v1.0.0
-
+export gdas_ver=v15.0.0
+export global_shared_ver=v15.0.0
+export gdas_conmon_ver=v1.0.0
+export conmon_shared_ver=v1.0.0
 
 #############################################################
 # Load modules
@@ -43,6 +33,17 @@ module load prod_envir
 
 module list
 
+#############################################################
+# Specify whether the run is production or development
+#############################################################
+export PDY=`echo $PDATE | cut -c1-8`
+export CYC=`echo $PDATE | cut -c9-10`
+export conmon_job=${conmon_job:-gdas_conmon.${CYC}}
+export pid=${pid:-$$}
+export jobid=${conmon_job}.${pid}
+export DATAROOT=/gpfs/hps3/emc/da/noscrub/$LOGNAME/test_data
+export COMROOT=/gpfs/hps/ptmp/$LOGNAME/com
+
 
 #############################################################
 # WCOSS environment settings
@@ -53,38 +54,31 @@ export POE=YES
 #############################################################
 # Set user specific variables
 #############################################################
-export DATAROOT=${DATAROOT:-/gpfs/hps/emc/da/noscrub/$LOGNAME/test_data}
-#export COMROOT=${COMROOT:-/gpfs/hps/ptmp/$LOGNAME/com}
-export PTMP_USER=${PTMP_USER:-/gpfs/hps/ptmp/$LOGNAME}
-export CMON_SUFFIX=${CMON_SUFFIX:-testcmon}
-export NWTEST=${NWTEST:-/gpfs/hps/emc/da/noscrub/${LOGNAME}/CMon_486/util/Conventional_Monitor/nwprod}
-export HOMEgdascmon=${HOMEgdascmon:-${NWTEST}/gdas_cmon.${gdas_cmon_ver}}
-export JOBGLOBAL=${JOBGLOBAL:-${HOMEgdascmon}/jobs}
-export HOMEcmon=${HOMEcmon:-${NWTEST}/cmon_shared.${cmon_shared_ver}}
-export COM_IN=${COM_IN:-${DATAROOT}}
+export CONMON_SUFFIX=${CONMON_SUFFIX:-test_conmon}
+export NWTEST=${NWTEST:-/gpfs/hps3/emc/da/noscrub/${LOGNAME}/ProdGSI/util/Conventional_Monitor/nwprod}
+export HOMEconmon=${HOMEconmon:-${NWTEST}/conmon_shared.${conmon_shared_ver}}
+export HOMEgdas_conmon=${HOMEgdas_conmon:-${NWTEST}/gdas_conmon.${gdas_conmon_ver}}
+export HOMEgfs_conmon=${HOMEgdas_conmon}
 
-export C_TANKDIR=${C_TANKDIR:-${PTMP_USER}/com/${CMON_SUFFIX}}
+export JOBGLOBAL=${HOMEgdas_conmon}/jobs
+export C_COM_IN=${C_COM_IN:-${DATAROOT}}
+
+
+export C_TANKDIR=${C_TANKDIR:-${COMROOT}/${CONMON_SUFFIX}}
 if [[ ! -d $C_TANKDIR ]]; then
    mkdir -p ${C_TANKDIR}
 fi
 
-export CMON_WORK_DIR=${CMON_WORK_DIR:-/gpfs/hps/stmp/$LOGNAME/cmon_${CMON_SUFFIX}}
-if [[ ! -d $CMON_WORK_DIR ]]; then
-   mkdir -p ${CMON_WORK_DIR}
+export CONMON_WORK_DIR=${CONMON_WORK_DIR:-/gpfs/hps/stmp/$LOGNAME/conmon_${CONMON_SUFFIX}}
+if [[ ! -d $CONMON_WORK_DIR ]]; then
+   mkdir -p ${CONMON_WORK_DIR}
 fi
 
-export CMON_LOG_DIR=${CMON_LOG_DIR:-${PTMP_USER}/logs/jlogfiles}
-if [[ ! -d $CMON_LOG_DIR ]]; then
-   mkdir -p ${CMON_LOG_DIR}
-fi
-
-export jlogfile=${jlogfile:-${CMON_LOG_DIR}/jlogfile.${jobid}}
-export utilroot=${utilroot:-$PROD}
 
 #############################################################
 # Execute job
 #############################################################
-$JOBGLOBAL/JGDAS_VCMON
+$JOBGLOBAL/JGDAS_CONMON
 
 exit
 
