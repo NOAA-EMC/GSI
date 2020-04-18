0a565f3ff06595d7fa8fe548e5e1835adb0ef35f
jswhit2 <Jeffrey.S.Whitaker@noaa.gov>
2020-04-18 01:40:38 +0000

    remove MPI3FLAG - no longer used

--- cmake/Modules/platforms/WCOSS-D.cmake
+++ cmake/Modules/platforms/WCOSS-D.cmake
@@ -7,7 +7,7 @@ macro (setWCOSS_D)
   set(MKL_FLAG "-mkl"  CACHE INTERNAL "MKL Flag")
   set(GSI_Intel_Platform_FLAGS "-DPOUND_FOR_STRINGIFY -fp-model strict -assume byterecl -convert big_endian -implicitnone -D_REAL8_ ${OpenMP_Fortran_FLAGS} ${MPI_Fortran_COMPILE_FLAGS} -O3" CACHE INTERNAL "GSI Fortran Flags")
   set(GSI_LDFLAGS "${OpenMP_Fortran_FLAGS} ${MKL_FLAG}" CACHE INTERNAL "")
-  set(ENKF_Platform_FLAGS "-O3 -fp-model strict -convert big_endian -assume byterecl -implicitnone  -DGFS -D_REAL8_ ${MPI3FLAG} ${OpenMP_Fortran_FLAGS} " CACHE INTERNAL "ENKF Fortran Flags")
+  set(ENKF_Platform_FLAGS "-O3 -fp-model strict -convert big_endian -assume byterecl -implicitnone  -DGFS -D_REAL8_  ${OpenMP_Fortran_FLAGS} " CACHE INTERNAL "ENKF Fortran Flags")
 
   set(HDF5_USE_STATIC_LIBRARIES "ON" CACHE INTERNAL "" )
   if( NOT DEFINED ENV{COREPATH} )
