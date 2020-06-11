d1082a1f4d020e707869cee88ff083bc57e801c8
edward.safford <edward.safford@noaa.gov>
2020-06-11 16:40:32 +0000

    GitHub Issue NOAA-EMC/GSI #3.  Port RadMon to Hera.

--- util/Radiance_Monitor/nwprod/gdas_radmon.v3.0.0/driver/test_jgdas_verfrad_hera.sh
+++ util/Radiance_Monitor/nwprod/gdas_radmon.v3.0.0/driver/test_jgdas_verfrad_hera.sh
@@ -10,8 +10,11 @@
 
 set -x
 
+export MY_MACHINE=hera
+
 #export PDATE=${PDATE:-2018091712}	#binary
 export PDATE=${PDATE:-2018110206}	#NetCDF
+
 #############################################################
 # Specify whether the run is production or development
 #############################################################
@@ -21,8 +24,8 @@ export job=gdas_verfrad.${cyc}
 export pid=${pid:-$$}
 export jobid=${job}.${pid}
 export envir=para
-export DATAROOT=${DATAROOT:-/scratch4/NCEPDEV/da/noscrub/Edward.Safford/test_data}
-export COMROOT=${COMROOT:-/scratch4/NCEPDEV/stmp3/$LOGNAME/com}
+export DATAROOT=${DATAROOT:-/scratch1/NCEPDEV/da/Edward.Safford/noscrub/test_data}
+export COMROOT=${COMROOT:-/scratch2/NCEPDEV/stmp3/${LOGNAME}/com}
 
 
 #############################################################
@@ -35,20 +38,11 @@ export radmon_shared_ver=v3.0.0
 
 
 #############################################################
-# Add nwpara tools to path
-#############################################################
-NWPROD=${NWPROD:-/scratch4/NCEPDEV/global/save/glopara/nwpara/util}
-NWPRODush=${NWPRODush:=${NWPROD}/ush}
-NWPRODexec=${NWPRODexec:=${NWPROD}/exec}
-export PATH=${PATH}:${NWPRODush}:${NWPRODexec}
-
-#############################################################
 # Set user specific variables
 #############################################################
 
 export RADMON_SUFFIX=${RADMON_SUFFIX:-testrad}
-#export NWTEST=${NWTEST:-/scratch4/NCEPDEV/da/noscrub/${LOGNAME}/gfs_q3fy17}
-export NWTEST=${NWTEST:-/scratch4/NCEPDEV/da/noscrub/Edward.Safford/ProdGSI/util/Radiance_Monitor/nwprod}
+export NWTEST=${NWTEST:-/scratch1/NCEPDEV/da/Edward.Safford/noscrub/GSI/util/Radiance_Monitor/nwprod}
 
 export HOMEgdas=${HOMEgdas:-${NWTEST}/gdas_radmon.${gdas_radmon_ver}}
 export HOMEgfs=$HOMEgdas
@@ -59,28 +53,19 @@ export HOMEradmon=${HOMEradmon:-${NWTEST}/radmon_shared.${radmon_shared_ver}}
 export COM_IN=${COM_IN:-${DATAROOT}}
 export TANKverf=${TANKverf:-${COMROOT}/${RADMON_SUFFIX}}
 
-export SUB=${SUB:-/apps/torque/default/bin/qsub}
-export NDATE=${NDATE:-ndate}
+export SUB=${SUB:-/apps/slurm/default/bin/sbatch}
+export NDATE=${NDATE:-/home/Edward.Safford/bin/ndate}
 
 export parm_file=${HOMEgdas}/parm/gdas_radmon.parm
 
-#######################################################################
-#  theia specific hacks for no prod_utils module & no setpdy.sh script
-#######################################################################
-export MY_MACHINE=theia
+
 prevday=`$NDATE -24 $PDATE`
 export PDYm1=`echo $prevday | cut -c1-8`
-ln -s ${NWPRODush}/startmsg.sh ${COMROOT}/startmsg
-ln -s ${NWPRODush}/postmsg.sh ${COMROOT}/postmsg
-ln -s ${NWPRODush}/prep_step.sh ${COMROOT}/prep_step
-ln -s ${NWPRODush}/err_chk.sh ${COMROOT}/err_chk
-export PATH=$PATH:${COMROOT}
-export utilscript=${utilscript:-${NWPRODush}}		# err_chk calls postmsg.sh
-							#   directly so need to override
-							#   utilscript location for theia
+						
+	
 #############################################################
 # Execute job
-#############################################################
+#
 $JOBGLOBAL/JGDAS_VERFRAD
 
 exit
