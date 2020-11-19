de65128ca84ff6820269e747ac0185258ce85959
TingLei-NOAA <Ting.Lei@gmail.com>
2020-11-19 20:00:38 +0000

    some cleaning

--- src/enkf/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /scratch4/NCEPDEV/fv3-cam/save/Ting.Lei/dr-CAM/dr-witaker/ProdGSI/src/enkf
-# Build directory: /scratch4/NCEPDEV/fv3-cam/save/Ting.Lei/dr-CAM/dr-witaker/ProdGSI/src/enkf
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
