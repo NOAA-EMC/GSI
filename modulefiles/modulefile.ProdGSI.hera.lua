76f2c478cb0328f463cbd556a51ff5518b22d1ba
michael.lueken <Michael.Lueken@noaa.gov>
2022-02-16 14:47:57 +0000

    GitHub Issue NOAA-EMC/GSI#298. Update modulefile/modulefile.ProdGSI.hera.lua file, moving cmake entry to after setuping hpc-stack path in order for the global-workflow to use cmake/3.20.0 from hpc-stack.

--- modulefiles/modulefile.ProdGSI.hera.lua
+++ modulefiles/modulefile.ProdGSI.hera.lua
@@ -2,9 +2,6 @@ help([[
 Load environment to compile GSI on Hera
 ]])
 
-cmake_ver=os.getenv("cmake_ver") or "3.16.1"
-load(pathJoin("cmake", cmake_ver))
-
 prepend_path("MODULEPATH", "/scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack/modulefiles/stack")
 
 hpc_ver=os.getenv("hpc_ver") or "1.1.0"
@@ -15,6 +12,9 @@ load(pathJoin("hpc-intel", hpc_intel_ver))
 
 impi_ver=os.getenv("hpc_impi_ver") or "2018.0.4"
 load(pathJoin("hpc-impi", hpc_impi_ver))
+
+cmake_ver=os.getenv("cmake_ver") or "3.16.1"
+load(pathJoin("cmake", cmake_ver))
 
 prepend_path("MODULEPATH", "/contrib/anaconda/modulefiles")
 
