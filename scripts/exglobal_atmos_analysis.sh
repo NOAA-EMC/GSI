a0f1aa18cce944da983de319ef7c0a87a5547843
michael.lueken <Michael.Lueken@noaa.gov>
2022-02-22 15:19:32 +0000

    GitHub Issue NOAA-EMC/GSI#246. Updated:  * fix - point to updated DO-4 fix files  * modulefiles/modulefile.ProdGSI.wcoss2.lua -     Use w3emc/2.9.2 and move cmake to after hpc-stack  * scripts/exgdas_enkf_sfc.sh -     Replace COMIN_OBS with COMIN     Replace COMIN_GES_OBS with COMIN_GES  * scripts/exglobal_atmos_analysis.sh -     Replace COMIN_OBS with COMIN     Replace COMIN_GES_OBS with COMIN_GES

--- scripts/exglobal_atmos_analysis.sh
+++ scripts/exglobal_atmos_analysis.sh
@@ -973,11 +973,11 @@ if [ $DOGCYCLE = "YES" ]; then
 
     # Global cycle requires these files
     export FNTSFA=${FNTSFA:-$COMIN_OBS/${OPREFIX}rtgssthr.grb}
-    export FNACNA=${FNACNA:-$COMIN_OBS/${OPREFIX}seaice.5min.blend.grb}
-    export FNSNOA=${FNSNOA:-$COMIN_OBS/${OPREFIX}snogrb_t${JCAP_CASE}.${LONB_CASE}.${LATB_CASE}}
-    [[ ! -f $FNSNOA ]] && export FNSNOA="$COMIN_OBS/${OPREFIX}snogrb_t1534.3072.1536"
-    FNSNOG=${FNSNOG:-$COMIN_GES_OBS/${GPREFIX}snogrb_t${JCAP_CASE}.${LONB_CASE}.${LATB_CASE}}
-    [[ ! -f $FNSNOG ]] && FNSNOG="$COMIN_GES_OBS/${GPREFIX}snogrb_t1534.3072.1536"
+    export FNACNA=${FNACNA:-$COMIN/${OPREFIX}seaice.5min.blend.grb}
+    export FNSNOA=${FNSNOA:-$COMIN/${OPREFIX}snogrb_t${JCAP_CASE}.${LONB_CASE}.${LATB_CASE}}
+    [[ ! -f $FNSNOA ]] && export FNSNOA="$COMIN/${OPREFIX}snogrb_t1534.3072.1536"
+    FNSNOG=${FNSNOG:-$COMIN_GES/${GPREFIX}snogrb_t${JCAP_CASE}.${LONB_CASE}.${LATB_CASE}}
+    [[ ! -f $FNSNOG ]] && FNSNOG="$COMIN_GES/${GPREFIX}snogrb_t1534.3072.1536"
 
     # Set CYCLVARS by checking grib date of current snogrb vs that of prev cycle
     if [ $RUN_GETGES = "YES" ]; then
