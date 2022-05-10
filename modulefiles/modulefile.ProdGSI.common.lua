01a1c95fe0ae611adf65a1c054e08c459d9b7441
Rahul Mahajan <aerorahul@users.noreply.github.com>
2022-05-09 23:22:34 -0400

    modulefile.ProdGSI.common.lua was buggy.  a source code change slipped through via one of the many merged PRs

--- modulefiles/modulefile.ProdGSI.common.lua
+++ modulefiles/modulefile.ProdGSI.common.lua
@@ -2,21 +2,21 @@ help([[
 Load common modules to build GSI on all machines
 ]])
 
-prod_util_ver=os.getenv("prod_util_ver") or "1.2.2"
+local prod_util_ver=os.getenv("prod_util_ver") or "1.2.2"
 
-netcdf_ver=os.getenv("netcdf_ver") or "4.7.4"
+local netcdf_ver=os.getenv("netcdf_ver") or "4.7.4"
 
-bufr_ver=os.getenv("bufr_ver") or "11.5.0"
-bacio_ver=os.getenv("bacio_ver") or "2.4.1"
-w3emc_ver=os.getenv("w3emc_ver") or "2.9.1"
-sp_ver=os.getenv("sp_ver") or "2.3.3"
-sp_ver=os.getenv("ip_ver") or "3.3.3"
-sigio_ver=os.getenv("sigio_ver") or "2.3.2"
-sigio_ver=os.getenv("sfcio_ver") or "1.4.1"
-sigio_ver=os.getenv("nemsio_ver") or "2.5.2"
-sigio_ver=os.getenv("wrf_io_ver") or "1.2.0"
-sigio_ver=os.getenv("ncio_ver") or "1.0.2"
-sigio_ver=os.getenv("crtm_ver") or "2.3.0"
+local bufr_ver=os.getenv("bufr_ver") or "11.5.0"
+local bacio_ver=os.getenv("bacio_ver") or "2.4.1"
+local w3emc_ver=os.getenv("w3emc_ver") or "2.9.1"
+local sp_ver=os.getenv("sp_ver") or "2.3.3"
+local ip_ver=os.getenv("ip_ver") or "3.3.3"
+local sigio_ver=os.getenv("sigio_ver") or "2.3.2"
+local sfcio_ver=os.getenv("sfcio_ver") or "1.4.1"
+local nemsio_ver=os.getenv("nemsio_ver") or "2.5.2"
+local wrf_io_ver=os.getenv("wrf_io_ver") or "1.2.0"
+local ncio_ver=os.getenv("ncio_ver") or "1.0.0"
+local crtm_ver=os.getenv("crtm_ver") or "2.3.0"
 
 load(pathJoin("prod_util", prod_util_ver))
 
