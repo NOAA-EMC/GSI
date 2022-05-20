706b2c72e73921b4f400651f077c56fc217c0508
Bin Liu <Bin.Liu@noaa.gov>
2022-05-20 04:09:13 +0000

    Properly use mpimod, only: mpi_comm_world in src/enkf/gridinfo_fv3reg.f90. Otherwise, the build on wcoss2 will fail.

--- src/enkf/gridinfo_fv3reg.f90
+++ src/enkf/gridinfo_fv3reg.f90
@@ -42,7 +42,8 @@ module gridinfo
 !
 !$$$
 
-use mpisetup, only: nproc, mpi_integer, mpi_real4, mpi_comm_world
+use mpimod, only: mpi_comm_world
+use mpisetup, only: nproc, mpi_integer, mpi_real4
 use params, only: datapath,nlevs,nlons,nlats,use_gfs_nemsio, fgfileprefixes, &
                   fv3fixpath, nx_res,ny_res, ntiles,l_fv3reg_filecombined
 use kinds, only: r_kind, i_kind, r_double, r_single
