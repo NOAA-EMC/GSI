0a289712e28fb672f2b3d8398733549be9c78aa3
russ.treadon <Russ.Treadon@noaa.gov>
2022-09-07 19:32:59 +0000

    Revert update to PrgEnv_intel, craype, cray_mpich versions in gsi_wcoss2.lua (#474)

--- modulefiles/gsi_wcoss2.lua
+++ modulefiles/gsi_wcoss2.lua
@@ -1,10 +1,10 @@
 help([[
 ]])
 
-local PrgEnv_intel_ver=os.getenv("PrgEnv_intel_ver") or "8.2.0"
+local PrgEnv_intel_ver=os.getenv("PrgEnv_intel_ver") or "8.1.0"
 local intel_ver=os.getenv("intel_ver") or "19.1.3.304"
-local craype_ver=os.getenv("craype_ver") or "2.7.13"
-local cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.12"
+local craype_ver=os.getenv("craype_ver") or "2.7.8"
+local cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.7"
 local cmake_ver= os.getenv("cmake_ver") or "3.20.2"
 local python_ver=os.getenv("python_ver") or "3.8.6"
 local prod_util_ver=os.getenv("prod_util_ver") or "2.0.10"
