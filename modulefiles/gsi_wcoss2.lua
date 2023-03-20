f70d7a8fc3ab77c20212e2c0d8738586f23e6f2e
Bin Liu <Bin.Liu@noaa.gov>
2023-03-20 22:38:14 +0000

    Revert "Update modulefiles/gsi_wcoss2.lua to use WCOSS2 production area modules. (From @BijuThomas-NOAA)"
    This reverts commit a377fc56233e56605d6ddb66dcc91035876623dd.

--- modulefiles/gsi_wcoss2.lua
+++ modulefiles/gsi_wcoss2.lua
@@ -8,7 +8,6 @@ local cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.7"
 local cmake_ver= os.getenv("cmake_ver") or "3.20.2"
 local python_ver=os.getenv("python_ver") or "3.8.6"
 local prod_util_ver=os.getenv("prod_util_ver") or "2.0.10"
-local ncio_ver=os.getenv("ncio_ver") or "1.0.0"
 
 load(pathJoin("PrgEnv-intel", PrgEnv_intel_ver))
 load(pathJoin("intel", intel_ver))
@@ -20,14 +19,14 @@ load(pathJoin("python", python_ver))
 load(pathJoin("prod_util", prod_util_ver))
 
 load("gsi_common")
+unload("ncio")
 unload("ncdiag")
-unload ("ncio")
-load(pathJoin("ncio", ncio_ver))
 
 pushenv("HPC_OPT", "/apps/ops/para/libs")
 prepend_path("MODULEPATH", "/apps/ops/para/libs/modulefiles/compiler/intel/19.1.3.304")
 prepend_path("MODULEPATH", "/apps/ops/para/libs/modulefiles/mpi/intel/19.1.3.304/cray-mpich/8.1.7")
 
+load("ncio/1.1.2")
 load("ncdiag/1.0.0")
 
 whatis("Description: GSI environment on WCOSS2")
