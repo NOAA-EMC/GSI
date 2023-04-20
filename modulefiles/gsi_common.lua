ec72190c6e75e0c97b440f9dad9a2fd55559bb12
Emily Liu <emily.liu@noaa.gov>
2023-04-20 15:45:06 +0000

    Modify hpc stack to point to hpc-stack-gfsv16 since hpc-stack is outdated.

--- modulefiles/gsi_common.lua
+++ modulefiles/gsi_common.lua
@@ -6,7 +6,8 @@ local netcdf_ver=os.getenv("netcdf_ver") or "4.7.4"
 
 local bufr_ver=os.getenv("bufr_ver") or "11.7.0"
 local bacio_ver=os.getenv("bacio_ver") or "2.4.1"
-local w3emc_ver=os.getenv("w3emc_ver") or "2.9.1"
+local w3emc_ver=os.getenv("w3emc_ver") or "2.9.2"
+local w3nco_ver=os.getenv("w3nco_ver") or "2.4.1"
 local sp_ver=os.getenv("sp_ver") or "2.3.3"
 local ip_ver=os.getenv("ip_ver") or "3.3.3"
 local sigio_ver=os.getenv("sigio_ver") or "2.3.2"
@@ -21,6 +22,7 @@ load(pathJoin("netcdf", netcdf_ver))
 load(pathJoin("bufr", bufr_ver))
 load(pathJoin("bacio", bacio_ver))
 load(pathJoin("w3emc", w3emc_ver))
+load(pathJoin("w3nco", w3nco_ver))
 load(pathJoin("sp", sp_ver))
 load(pathJoin("ip", ip_ver))
 load(pathJoin("sigio", sigio_ver))
