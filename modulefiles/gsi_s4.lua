437d6fb4d94b3ecc67310f436751fa0593faae4d
David Huber <david.huber@noaa.gov>
2023-05-10 15:17:35 +0000

    Update S4 module, remove TODO. #447

--- modulefiles/gsi_s4.lua
+++ modulefiles/gsi_s4.lua
@@ -1,9 +1,9 @@
 help([[
 ]])
 
-local hpc_ver=os.getenv("hpc_ver") or "1.1.0"
-local hpc_intel_ver=os.getenv("hpc_intel_ver") or "18.0.4"
-local hpc_impi_ver=os.getenv("hpc_impi_ver") or "18.0.4"
+local hpc_ver=os.getenv("hpc_ver") or "1.2.0"
+local hpc_intel_ver=os.getenv("hpc_intel_ver") or "2022.1"
+local hpc_impi_ver=os.getenv("hpc_impi_ver") or "2022.1"
 local miniconda_ver=os.getenv("miniconda_ver") or "3.8-s4"
 local prod_util_ver=os.getenv("prod_util_ver") or "1.2.2"
 
