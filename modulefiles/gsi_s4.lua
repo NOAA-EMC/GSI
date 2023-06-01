11d6cea63488df9fbb029c2e6b83c8e4b793a89c
RussTreadon-NOAA <Russ.Treadon@noaa.gov>
2023-06-01 10:24:01 +0000

    update GSI_BINARY_SOURCE_DIR in modulefiles to point at 20230601 (#580)

--- modulefiles/gsi_s4.lua
+++ modulefiles/gsi_s4.lua
@@ -23,6 +23,6 @@ load(pathJoin("prod_util", prod_util_ver))
 pushenv("CFLAGS", "-march=ivybridge")
 pushenv("FFLAGS", "-march=ivybridge")
 
-pushenv("GSI_BINARY_SOURCE_DIR", "/data/prod/glopara/fix/gsi/20230112")
+pushenv("GSI_BINARY_SOURCE_DIR", "/data/prod/glopara/fix/gsi/20230601")
 
 whatis("Description: GSI environment on S4 with Intel Compilers")
