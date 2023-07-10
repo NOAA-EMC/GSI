d0fdffcd664c04a0aadbb0a04a8322e9c94a0efa
RussTreadon-NOAA <Russ.Treadon@noaa.gov>
2023-07-10 19:12:27 +0000

    update gsi_wcoss2.lua to ncdiag/1.1.1 and nemsio/2.5.4, update gnu compilers from PR #584

--- modulefiles/gsi_wcoss2.lua
+++ modulefiles/gsi_wcoss2.lua
@@ -4,10 +4,15 @@ help([[
 local PrgEnv_intel_ver=os.getenv("PrgEnv_intel_ver") or "8.1.0"
 local intel_ver=os.getenv("intel_ver") or "19.1.3.304"
 local craype_ver=os.getenv("craype_ver") or "2.7.8"
-local cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.7"
+local cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.9"
 local cmake_ver= os.getenv("cmake_ver") or "3.20.2"
 local python_ver=os.getenv("python_ver") or "3.8.6"
 local prod_util_ver=os.getenv("prod_util_ver") or "2.0.10"
+
+prepend_path("MODULEPATH", "/lfs/h1/emc/nceplibs/noscrub/hpc-stack/libs/hpc-stack/modulefiles/stack")
+local hpc_ver=os.getenv("hpc_ver") or "1.2.0"
+local hpc_intel_ver=os.getenv("hpc_intel_ver") or "19.1.3.304"
+local hpc_cray_mpich_ver=os.getenv("hpc_cray_mpich_ver") or "8.1.9"
 
 load(pathJoin("PrgEnv-intel", PrgEnv_intel_ver))
 load(pathJoin("intel", intel_ver))
@@ -17,6 +22,10 @@ load(pathJoin("cmake", cmake_ver))
 load(pathJoin("python", python_ver))
 
 load(pathJoin("prod_util", prod_util_ver))
+
+load(pathJoin("hpc", hpc_ver))
+load(pathJoin("hpc-intel", hpc_intel_ver))
+load(pathJoin("hpc-cray-mpich", hpc_cray_mpich_ver))
 
 load("gsi_common")
 
