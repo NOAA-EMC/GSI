b708ffc330a1ad131749aa4f9c16d6ff2e583d40
DavidHuber <David.Huber@noaa.gov>
2023-07-31 15:56:25 +0000

    Updated noaa-cloud spack-stack location #589.

---
+++ modulefiles/gsi_noaacloud.lua
@@ -0,0 +1,20 @@
+help([[
+]])
+
+prepend_path("MODULEPATH", "/contrib/EPIC/spack-stack/spack-stack-1.4.1/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/contrib/spack-stack/modulefiles/core")
+
+local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.3.0"
+local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.3.0"
+local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
+local stack_python_ver=os.getenv("stack_python_ver") or "3.9.12"
+
+load(pathJoin("stack-intel", stack_intel_ver))
+load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+load(pathJoin("cmake", cmake_ver))
+load(pathJoin("stack-python", stack_python_ver))
+
+load("gsi_common")
+
+pushenv("GSI_BINARY_SOURCE_DIR", "/contrib/EPIC/GSI_data/fix/20230601")
+whatis("Description: GSI environment on Cloud with Intel Compilers")
