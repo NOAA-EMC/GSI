aff4365270746ca5e4d9594da2aade4689e65617
Russ-Treadon-NOAA <Russ.Treadon@noaa.gov>
2023-08-21 18:02:23 +0000

    add version numbers to module loads in gsi_common_wcoss2.lua (#447)

--- modulefiles/gsi_common_wcoss2.lua
+++ modulefiles/gsi_common_wcoss2.lua
@@ -2,11 +2,12 @@ help([[
 Load common modules to build GSI on all machines
 ]])
 
-local netcdf_ver=os.getenv("netcdf_ver") or "4.7.4"
+local netcdf_c_ver=os.getenv("netcdf_c_ver") or "4.9.2"
+local netcdf_fortran_ver=os.getenv("netcdf_fortran_ver") or "4.6.0"
 
 local bufr_ver=os.getenv("bufr_ver") or "11.7.0"
 local bacio_ver=os.getenv("bacio_ver") or "2.4.1"
-local w3emc_ver=os.getenv("w3emc_ver") or "2.9.1"
+local w3emc_ver=os.getenv("w3emc_ver") or "2.9.2"
 local sp_ver=os.getenv("sp_ver") or "2.3.3"
 local ip_ver=os.getenv("ip_ver") or "3.3.3"
 local sigio_ver=os.getenv("sigio_ver") or "2.3.2"
@@ -17,19 +18,20 @@ local ncio_ver=os.getenv("ncio_ver") or "1.1.2"
 local crtm_ver=os.getenv("crtm_ver") or "2.4.0"
 local ncdiag_ver=os.getenv("ncdiag_ver") or "1.1.1"
 
-load("netcdf-c")
-load("netcdf-fortran")
+load(pathJoin("netcdf-c", netcdf_c_ver))
+load(pathJoin("netcdf-fortran", netcdf_fortran_ver))
 
-load("bufr/11.7.0")
-load("bacio")
-load("w3emc")
-load("sp")
-load("ip/3.3.3")
-load("sigio")
-load("sfcio")
-load("nemsio")
-load("wrf-io")
-load("ncio")
-load("crtm")
+
+load(pathJoin("bufr", bufr_ver))
+load(pathJoin("bacio", bacio_ver))
+load(pathJoin("w3emc", w3emc_ver))
+load(pathJoin("sp", sp_ver))
+load(pathJoin("ip", ip_ver))
+load(pathJoin("sigio", sigio_ver))
+load(pathJoin("sfcio", sfcio_ver))
+load(pathJoin("nemsio", nemsio_ver))
+load(pathJoin("wrf_io", wrf_io_ver))
+load(pathJoin("ncio", ncio_ver))
+load(pathJoin("crtm", crtm_ver))
 load(pathJoin("gsi-ncdiag",ncdiag_ver))
 
