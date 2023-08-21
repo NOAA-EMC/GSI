0fd3489bcd809db3352900f2763804c6b60e2405
Russ-Treadon-NOAA <Russ.Treadon@noaa.gov>
2023-08-21 14:38:49 +0000

    modifications for acorn (wcoss2 test system) build using intel19 spack-stack (#447)

--- modulefiles/gsi_wcoss2.lua
+++ modulefiles/gsi_wcoss2.lua
@@ -1,23 +1,20 @@
 help([[
 ]])
 
-prepend_path("MODULEPATH", "/lfs/h1/emc/nceplibs/noscrub/spack-stack/spack-stack-1.4.1/envs/unified-env/install/modulefiles/Core")
-
-load("stack-intel")
-load("stack-cray-mpich")
-load("stack-python")
-
---Avoid production installations; use spack-stack only:
-remove_path("MODULEPATH", "/apps/ops/prod/libs/modulefiles/compiler/intel/19.1.3.304")
-remove_path("MODULEPATH", "/apps/ops/prod/libs/modulefiles/mpi/intel/19.1.3.304/cray-mpich/8.1.4")
-remove_path("MODULEPATH", "/apps/ops/prod/libs/modulefiles/mpi/intel/19.1.3.304/cray-mpich/8.1.7")
-remove_path("MODULEPATH", "/apps/prod/lmodules/intel/19.1.3.304")
-remove_path("MODULEPATH", "/apps/prod/lmodules/INTEL_cray_mpich/19.1.3.304/cray-mpich/8.1.4")
-
+load("PrgEnv-intel")
+load("intel")
+load("craype")
+load("cray-mpich")
 load("cmake")
 
-load("gsi_common")
+prepend_path("MODULEPATH", "/lfs/h1/emc/nceplibs/noscrub/Mark.Potts/spack-stack/spack-stack-1.4.1/envs/unified-dev-19/install/modulefiles/Core")
+load("stack-intel")
+load("stack-cray-mpich")
+load("prod-util")
+load("gsi_common_wcoss2")
 
+setenv("CC", "cc")
+setenv("CXX", "CC")
+setenv("FC", "ftn")
 pushenv("GSI_BINARY_SOURCE_DIR", "/lfs/h2/emc/global/noscrub/emc.global/FIX/fix/gsi/20230601")
-
-whatis("Description: GSI environment on WCOSS2")
+whatis("Description: GSI environment on Hera with Intel Compilers")
