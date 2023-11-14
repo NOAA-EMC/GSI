28ebd5f67bfe6fb6088024e3d7f527546678b6be
DavidHuber <david.huber@noaa.gov>
2023-11-14 17:04:12 +0000

    Update Hera GNU modulefile to spack-stack/1.5.1. #589

--- modulefiles/gsi_hera.gnu.lua
+++ modulefiles/gsi_hera.gnu.lua
@@ -1,10 +1,11 @@
 help([[
 ]])
 
-prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.4.1/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.5.1/envs/gsi-addon/install/modulefiles/Core")
+--Needed for openmpi build
 prepend_path("MODULEPATH", "/scratch1/NCEPDEV/jcsda/jedipara/spack-stack/modulefiles")
 
-local stack_python_ver=os.getenv("stack_python_ver") or "3.9.12"
+local python_ver=os.getenv("python_ver") or "3.10.8"
 local stack_gnu_ver=os.getenv("stack_gnu_ver") or "9.2.0"
 local stack_openmpi_ver=os.getenv("stack_openmpi_ver") or "4.1.5"
 local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
@@ -13,12 +14,12 @@ local openblas_ver=os.getenv("openblas_ver") or "0.3.19"
 
 load(pathJoin("stack-gcc", stack_gnu_ver))
 load(pathJoin("stack-openmpi", stack_openmpi_ver))
-load(pathJoin("stack-python", stack_python_ver))
+load(pathJoin("python", python_ver))
 load(pathJoin("cmake", cmake_ver))
 
 load("gsi_common")
 
-load(pathJoin("prod-util", prod_util_ver))
+load(pathJoin("prod_util", prod_util_ver))
 load(pathJoin("openblas", openblas_ver))
 
 pushenv("GSI_BINARY_SOURCE_DIR", "/scratch1/NCEPDEV/global/glopara/fix/gsi/20230911")
