4deb40fddf29e3d986e7504944c895b5f42c22c4
DavidHuber <david.huber@noaa.gov>
2023-11-14 17:53:40 +0000

    Update Hera/intel spack-stack to 1.5.1. #589

--- modulefiles/gsi_hera.intel.lua
+++ modulefiles/gsi_hera.intel.lua
@@ -1,10 +1,9 @@
 help([[
 ]])
 
-prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.4.1/envs/unified-env/install/modulefiles/Core")
-prepend_path("MODULEPATH", "/scratch1/NCEPDEV/jcsda/jedipara/spack-stack/modulefiles")
+prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.5.1/envs/gsi-addon/install/modulefiles/Core")
 
-local stack_python_ver=os.getenv("stack_python_ver") or "3.9.12"
+local python_ver=os.getenv("python_ver") or "3.10.8"
 local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
 local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.1"
 local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
@@ -12,11 +11,11 @@ local prod_util_ver=os.getenv("prod_util_ver") or "1.2.2"
 
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
-load(pathJoin("stack-python", stack_python_ver))
+load(pathJoin("python", python_ver))
 load(pathJoin("cmake", cmake_ver))
 
 load("gsi_common")
-load(pathJoin("prod-util", prod_util_ver))
+load(pathJoin("prod_util", prod_util_ver))
 
 pushenv("CFLAGS", "-xHOST")
 pushenv("FFLAGS", "-xHOST")
