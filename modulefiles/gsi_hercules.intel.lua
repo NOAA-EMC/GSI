a906115ac3842cabbcb4cecccce10cbbc9ab0e08
David Huber <david.huber@noaa.gov>
2024-02-09 08:25:00 -0600

    Revert Hercules modules to SS/1.5.1.

--- modulefiles/gsi_hercules.intel.lua
+++ modulefiles/gsi_hercules.intel.lua
@@ -1,18 +1,23 @@
 help([[
 ]])
 
-prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.6.0/envs/gsi-addon-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.5.1/envs/gsi-addon/install/modulefiles/Core")
 
-local stack_python_ver=os.getenv("stack_python_ver") or "3.11.6"
+local stack_python_ver=os.getenv("stack_python_ver") or "3.10.8"
 local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.9.0"
 local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.9.0"
 local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
-local prod_util_ver=os.getenv("prod_util_ver") or "2.1.1"
+local prod_util_ver=os.getenv("prod_util_ver") or "1.2.2"
 
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 load(pathJoin("python", stack_python_ver))
 load(pathJoin("cmake", cmake_ver))
+
+-- netcdf-fortran/4.6.1 does not work on Hercules yet
+pushenv("netcdf_fortran_ver", "4.6.0")
+pushenv("sp_ver", "2.3.3")
+pushenv("crtm_ver", "2.4.0")
 
 load("gsi_common")
 load(pathJoin("prod_util", prod_util_ver))
