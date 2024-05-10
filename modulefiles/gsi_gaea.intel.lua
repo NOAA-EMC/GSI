c44a771d1dcdb9cef02d3cca575750e9899d2708
David Burrows <David.Burrows@noaa.gov>
2024-05-10 14:26:11 -0400

    module file update

--- modulefiles/gsi_gaea.intel.lua
+++ modulefiles/gsi_gaea.intel.lua
@@ -4,7 +4,7 @@ help([[
 prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
 prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/spack-stack-1.6.0/envs/gsi-addon-dev/install/modulefiles/Core")
 
-local python_ver=os.getenv("python_ver") or "3.11.6"
+local stack_python_ver=os.getenv("stack_python_ver") or "3.11.6"
 local stack_intel_ver=os.getenv("stack_intel_ver") or "2023.1.0"
 local stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.25"
 local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
@@ -12,12 +12,23 @@ local prod_util_ver=os.getenv("prod_util_ver") or "2.1.1"
 
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
-load(pathJoin("python", python_ver))
+load(pathJoin("stack-python", stack_python_ver))
 load(pathJoin("cmake", cmake_ver))
 
 load("gsi_common")
 load(pathJoin("prod_util", prod_util_ver))
 
+local MKLROOT="/opt/intel/oneapi/mkl/2022.0.2/"
+prepend_path("LD_LIBRARY_PATH",pathJoin(MKLROOT,"lib/intel64"))
+pushenv("MKLROOT", MKLROOT)
+
+unload("cray-python")
+unload("darshan-runtime")
+
 pushenv("GSI_BINARY_SOURCE_DIR", "/gpfs/f5/epic/proj-shared/global/glopara/data/fix/gsi/20240208")
 
-whatis("Description: GSI environment on Hera with Intel Compilers")
+setenv("CC","cc")
+setenv("FC","ftn")
+setenv("CXX","CC")
+pushenv("CRAYPE_LINK_TYPE","dynamic")
+whatis("Description: GSI environment on Gaea with Intel Compilers")
