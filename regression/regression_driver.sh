b0aa72d2e127db4e5988a61b8a965a33d08a361e
david huber <david.huber@noaa.gov>
2024-10-03 20:14:55 +0000

    Combine acorn/wcoss2 scripts as much as possible

--- regression/regression_driver.sh
+++ regression/regression_driver.sh
@@ -45,7 +45,7 @@ for jn in `seq ${RSTART} ${REND}`; do
    fi
    rm -f ${job[$jn]}.out
 
-   /bin/sh $ush/$sub_cmd -q $queue -j ${job[$jn]} -t ${topts[$jn]} -p ${popts[$jn]} -r ${ropts[$jn]} $scripts/${regtest}.sh
+   /bin/sh $ush/$sub_cmd -m ${machine} -q $queue -j ${job[$jn]} -t ${topts[$jn]} -p ${popts[$jn]} -r ${ropts[$jn]} $scripts/${regtest}.sh
 
    if [ $debug == ".true." ]; then break; fi
    $scripts/regression_wait.sh ${job[$jn]} ${rcname} $check_resource
