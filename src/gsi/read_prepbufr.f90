bfbca87ec0866ec592272ae348caf37a7bb13237
Andrew Collard <andrew.collard@noaa.gov>
2024-10-17 15:08:52 -0500

    Fix time window

--- src/gsi/read_prepbufr.f90
+++ src/gsi/read_prepbufr.f90
@@ -1156,8 +1156,11 @@ subroutine read_prepbufr(nread,ndata,nodata,infile,obstype,lunout,twindin,sis,&
               if (l4dvar.or.l4densvar) then
                  if ((t4dv<zero.OR.t4dv>winlen) .and. .not.driftl) cycle loop_readsb ! outside time window
               else
-                 if((real(abs(time)) > real(ctwind(nc)) .or. real(abs(time)) > real(twindin)) &
-                    .and. .not. driftl)cycle loop_readsb ! outside time window
+                 ! Fix to ensure end of time window is NOT included
+                 if ((real(abs(time)) > real(ctwind(nc)) .or. real(time)==real(three) .or. &
+                      real(abs(time)) > real(twindin)) .and. .not. driftl)cycle loop_readsb ! outside time window
+!                 if((real(abs(time)) > real(ctwind(nc)) .or. real(abs(time)) > real(twindin)) &
+!                    .and. .not. driftl)cycle loop_readsb ! outside time window
               endif
 
               timex=time
@@ -1918,7 +1921,10 @@ subroutine read_prepbufr(nread,ndata,nodata,infile,obstype,lunout,twindin,sis,&
                  if (l4dvar.or.l4densvar) then
                     if (t4dv<zero.OR.t4dv>winlen) cycle LOOP_K_LEVS
                  else
-                    if (real(abs(time))>real(ctwind(nc)) .or.  real(abs(time))>real(twindin)) cycle LOOP_K_LEVS
+                 ! Fix to ensure end of time window is NOT included
+                    if ((real(abs(time)) > real(ctwind(nc)) .or. real(time)==real(three) .or. &
+                      real(abs(time)) > real(twindin)) cycle LOOP_K_LEVS ! outside time window
+!                    if (real(abs(time))>real(ctwind(nc)) .or.  real(abs(time))>real(twindin)) cycle LOOP_K_LEVS
                  endif
               end if
 
