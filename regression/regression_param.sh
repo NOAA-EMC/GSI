3b98cf49df7877eafb3a470d0d4dcd3fa0a36445
DavidBurrows-NCO <David.Burrows@noaa.gov>
2024-11-08 08:43:29 -0500

    adjusting C6 params

--- regression/regression_param.sh
+++ regression/regression_param.sh
@@ -213,8 +213,8 @@ case $regtest in
            topts[1]="0:15:00" ; popts[1]="40/2/"  ; ropts[1]="/1"
            topts[2]="0:15:00" ; popts[2]="40/4/"  ; ropts[2]="/1"
         elif [[ "$machine" = "gaeac6" ]]; then
-           topts[1]="0:15:00" ; popts[1]="40/2/"  ; ropts[1]="/1"
-           topts[2]="0:15:00" ; popts[2]="40/4/"  ; ropts[2]="/1"
+           topts[1]="0:15:00" ; popts[1]="64/1/"  ; ropts[1]="/1"
+           topts[2]="0:15:00" ; popts[2]="64/2/"  ; ropts[2]="/1"
         elif [[ "$machine" = "wcoss2" || "$machine" = "acorn" ]]; then
            topts[1]="0:15:00" ; popts[1]="64/1/" ; ropts[1]="/1"
            topts[2]="0:15:00" ; popts[2]="64/2/" ; ropts[2]="/1"
