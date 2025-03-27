1aea9d4716821d69da4ff342523fe82abb0137de
Gang Zhao <gang.zhao@noaa.gov>
2025-03-27 12:15:41 +0000

    Reading the height of wind sensor for 3DRTMA for provider list (same as 2DRTMA). Updating the grid names for 3DRTMA CONUS domain and Alaska domain in subroutine ndfdgeid_info.

--- src/gsi/read_prepbufr.f90
+++ src/gsi/read_prepbufr.f90
@@ -897,14 +897,9 @@ subroutine read_prepbufr(nread,ndata,nodata,infile,obstype,lunout,twindin,sis,&
 
   if (lhilbert) call init_hilbertcurve(maxobs)
 
-  if (twodvar_regional) then
+  if (twodvar_regional .or. l_rtma3d) then
      call init_ndfdgrid
      call init_windht_lists !load wind sensor height provider lists
-  endif
-
-!  set up data for usage of valley map in 3DRTMA run
-  if (l_rtma3d) then
-     call init_ndfdgrid
   endif
 
 ! loop over convinfo file entries; operate on matches
