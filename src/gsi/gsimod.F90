14cf117d766e71e394ff24702fb409629285586b
ClaraDraper-NOAA <Clara.Draper@noaa.gov>
2025-04-17 21:19:06 +0000

    Output ignore_2mQM and hofx_2m_sfcfile to stdout.

--- src/gsi/gsimod.F90
+++ src/gsi/gsimod.F90
@@ -2317,6 +2317,8 @@
      write(6,strongopts)
      write(6,obsqc)
      write(6,*)'EXT_SONDE on type 120 =',ext_sonde
+     write(6,*)'hofx_2m_sfcfile =', hofx_2m_sfcfile
+     write(6,*)'ignore_2mQM =', ignore_2mQM
      ngroup=0
      do i=1,ndat
         dthin(i) = max(dthin(i),0)
