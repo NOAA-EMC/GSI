53adef8deafb2eaf96df7e3b4b953554988c2f8f
jianjunj <jianjun.jin@noaa.gov>
2025-04-29 14:33:45 +0000

    Code norm.

--- src/gsi/setuprad.f90
+++ src/gsi/setuprad.f90
@@ -1114,7 +1114,7 @@ contains
 
         predbias=zero
 
-!**!$omp parallel do  schedule(dynamic,1) private(i,mm,j,k,tlap,node,bias)
+!##!$omp parallel do  schedule(dynamic,1) private(i,mm,j,k,tlap,node,bias)
         do i=1,nchanl
            mm=ich(i)
 
@@ -1232,7 +1232,7 @@ contains
 
         kmax = 0
         if (lwrite_peakwt .or. passive_bc) then
-!**!$omp parallel do  schedule(dynamic,1) private(i,k,ptau5derivmax,ptau5deriv)
+!##!$omp parallel do  schedule(dynamic,1) private(i,k,ptau5derivmax,ptau5deriv)
            do i=1,nchanl
               ptau5derivmax = -9.9e31_r_kind
 ! maximum of weighting function is level at which transmittance
@@ -1258,7 +1258,7 @@ contains
         cld_rbc_idx2=zero
         if (radmod%lcloud_fwd .and. radmod%ex_biascor .and. eff_area) then
            ierrret=0
-!**!$omp parallel do  schedule(dynamic,1) private(i,mm,j)
+!##!$omp parallel do  schedule(dynamic,1) private(i,mm,j)
            do i=1,nchanl
               mm=ich(i)
               tsim_bc(i)=tsim(i)
@@ -1840,7 +1840,7 @@ contains
         account_for_corr_obs = .false.
         varinv0=zero
         raterr2 = zero
-!**!$omp parallel do  schedule(dynamic,1) private(ii,m,k,asum)
+!##!$omp parallel do  schedule(dynamic,1) private(ii,m,k,asum)
         do ii=1,nchanl
            m=ich(ii)
            if (varinv(ii)>tiny_r_kind .and. iuse_rad(m)>=1) then
