1396992e35a31bb2ae8898ac397c0873ab09c45a
matthew morris <matthew.t.morris@clogin04.cactus.wcoss2.ncep.noaa.gov>
2025-05-01 15:52:59 +0000

    Close if-statements in read_prepbufr.f90

--- src/gsi/read_prepbufr.f90
+++ src/gsi/read_prepbufr.f90
@@ -2086,6 +2086,8 @@ subroutine read_prepbufr(nread,ndata,nodata,infile,obstype,lunout,twindin,sis,&
                  if (sfcwndob_biasc) then
                     if ( kx==288.or.kx==295 .or. (gustob .and. (kx==188.or.kx==195)) )  then
                        call get_wbias_afactor(kx,obstype,c_station_id,c_prvstg,c_sprvstg,windbiasfact)
+                    endif
+                 endif
                  !retrieve wind sensor height for mesonet gustob only when running 3DRTMA
                  if (l_rtma3d)  then
                     if ( gustob .and. (kx==188.or.kx==195) )  then
