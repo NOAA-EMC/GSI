a92b3c6f84a26eaa9b5d22557692ffcc7870d83a
Russ-Treadon-NOAA <Russ.Treadon@noaa.gov>
2025-05-16 19:25:08 +0000

    Revert "Optimize global gsi for GFS v17 (#875)"
    This reverts commit 75b54e30adeac7638e28a65b7b7f9355e7679da4.

--- src/gsi/intall.f90
+++ src/gsi/intall.f90
@@ -192,7 +192,7 @@ subroutine intall(sval,sbias,rval,rbias)
   use gsi_bundlemod, only: gsi_bundle
   use gsi_bundlemod, only: assignment(=)
   use guess_grids, only: ntguessig,nfldsig
-  use mpi, only: mpi_in_place,mpi_real16,mpi_sum,mpi_comm_world
+  use mpl_allreducemod, only: mpl_allreduce
 
   implicit none
 
@@ -205,7 +205,7 @@ subroutine intall(sval,sbias,rval,rbias)
   real(r_quad),dimension(2*nobs_bins) :: mass
 
 ! Declare local variables
-  integer(i_kind) :: ibin,ii,it,i,ierr
+  integer(i_kind) :: ibin,ii,it,i
 
 !******************************************************************************
 ! Initialize timer
@@ -292,13 +292,13 @@ subroutine intall(sval,sbias,rval,rbias)
 
 !   Put reduces together to minimize wait time
 !   First, use MPI to get global mean increment
-    call MPI_Allreduce(mpi_in_place, mass, 2*nobs_bins, mpi_real16, mpi_sum, mpi_comm_world, ierr)
+    call mpl_allreduce(2*nobs_bins,qpvals=mass)
 
   end if
 
 ! Sum over all processors for bias correction terms
 
-  call MPI_Allreduce(mpi_in_place, qpred, nrclen, mpi_real16, mpi_sum, mpi_comm_world, ierr)
+  call mpl_allreduce(nrclen,qpvals=qpred)
 
 
 ! RHS for dry ps constraint: part 2
--- src/gsi/pcgsoi.f90
+++ src/gsi/pcgsoi.f90
@@ -159,7 +159,6 @@ subroutine pcgsoi()
   use berror, only: vprecond
   use stpjomod, only: stpjo_setup
   use intradmod, only: setrad
-  use mpi, only: mpi_in_place,mpi_real16,mpi_sum,mpi_comm_world
   
 
   implicit none
@@ -171,7 +170,7 @@ subroutine pcgsoi()
 ! Declare local variables  
   logical iout_6,restart,end_iter,llprt,llouter
   character(5) step(2)
-  integer(i_kind) i,istep,iobs,ii,nprt,ierr
+  integer(i_kind) i,istep,iobs,ii,nprt
   real(r_kind) stp,b,converge
   real(r_kind) gsave,small_step,aindex
   real(r_kind) gnormx,penx,penalty,penaltynew
@@ -356,8 +355,7 @@ subroutine pcgsoi()
            xdiff%values(i)=vprecond(i)*gradx%values(i)
         end do
         dprod(2) = qdot_prod_sub(xdiff,grady)
-        !call mpl_allreduce(4,qpvals=dprod)
-        call MPI_Allreduce(MPI_IN_PLACE, dprod, 4, MPI_REAL16, MPI_SUM, MPI_COMM_WORLD, ierr)
+        call mpl_allreduce(4,qpvals=dprod)
 !       Two dot products in dprod(3) and dprod(4) should be same, but are slightly
 !       different due to round off, so use average.
         gnorm(2)=dprod(2)-0.5_r_quad*(dprod(3)+dprod(4))
@@ -373,8 +371,7 @@ subroutine pcgsoi()
            xdiff%values(i)=vprecond(i)*gradx%values(i)
         end do
         dprod(2) = qdot_prod_sub(xdiff,grady)
-        !call mpl_allreduce(2,qpvals=dprod)
-        call MPI_Allreduce(MPI_IN_PLACE, dprod, 2, MPI_REAL16, MPI_SUM, MPI_COMM_WORLD, ierr)
+        call mpl_allreduce(2,qpvals=dprod)
         if(print_diag_pcg) call prt_control_norms(grady,'grady')
         gnorm(2)=dprod(2)
         gnorm(3)=dprod(2)
