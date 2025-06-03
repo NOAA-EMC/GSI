76cc38ad271ccc50b9ad1528e880e25468510524
David Huber <david.huber@noaa.gov>
2025-06-03 15:27:21 -0400

    Update to spack-stack 1.9.1 libraries everywhere

--- modulefiles/gsi_hercules.intel.lua
+++ modulefiles/gsi_hercules.intel.lua
@@ -1,13 +1,12 @@
 help([[
 ]])
 
-prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.6.0/envs/gsi-addon-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-1.9.1/envs/ue-oneapi-2024.1.0/install/modulefiles/Core")
 
 local stack_python_ver=os.getenv("stack_python_ver") or "3.11.6"
-local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.9.0"
-local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.9.0"
-local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
-local prod_util_ver=os.getenv("prod_util_ver") or "2.1.1"
+local stack_intel_ver=os.getenv("stack_intel_ver") or "2024.2.1"
+local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
+local cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
@@ -15,8 +14,7 @@ load(pathJoin("python", stack_python_ver))
 load(pathJoin("cmake", cmake_ver))
 
 load("gsi_common")
-load(pathJoin("prod_util", prod_util_ver))
-load("intel-oneapi-mkl/2022.2.1")
+load("intel-oneapi-mkl/2024.2.1")
 
 pushenv("CFLAGS", "-xHOST")
 pushenv("FFLAGS", "-xHOST")
--- modulefiles/gsi_wcoss2.intel.lua
+++ modulefiles/gsi_wcoss2.intel.lua
@@ -9,19 +9,18 @@ local cmake_ver= os.getenv("cmake_ver") or "3.20.2"
 local python_ver=os.getenv("python_ver") or "3.8.6"
 local prod_util_ver=os.getenv("prod_util_ver") or "2.0.10"
 
-local netcdf_ver=os.getenv("netcdf_ver") or "4.7.4"
+local netcdf_ver=os.getenv("netcdf_ver") or "4.9.2"
 local bufr_ver=os.getenv("bufr_ver") or "12.1.0"
 local bacio_ver=os.getenv("bacio_ver") or "2.4.1"
-local w3emc_ver=os.getenv("w3emc_ver") or "2.9.2"
-local sp_ver=os.getenv("sp_ver") or "2.3.3"
-local ip_ver=os.getenv("ip_ver") or "3.3.3"
-local sigio_ver=os.getenv("sigio_ver") or "2.3.2"
-local sfcio_ver=os.getenv("sfcio_ver") or "1.4.1"
+local w3emc_ver=os.getenv("w3emc_ver") or "2.10.0"
+local ip_ver=os.getenv("ip_ver") or "5.1.0"
+local sigio_ver=os.getenv("sigio_ver") or "2.3.3"
+local sfcio_ver=os.getenv("sfcio_ver") or "1.4.2"
 local nemsio_ver=os.getenv("nemsio_ver") or "2.5.4"
 local wrf_io_ver=os.getenv("wrf_io_ver") or "1.2.0"
 local ncio_ver=os.getenv("ncio_ver") or "1.1.2"
 local crtm_ver=os.getenv("crtm_ver") or "2.4.0.1"
-local ncdiag_ver=os.getenv("ncdiag_ver") or "1.1.1"
+local ncdiag_ver=os.getenv("ncdiag_ver") or "1.1.2"
 
 load(pathJoin("PrgEnv-intel", PrgEnv_intel_ver))
 load(pathJoin("intel", intel_ver))
@@ -32,11 +31,10 @@ load(pathJoin("python", python_ver))
 
 load(pathJoin("prod_util", prod_util_ver))
 
-load(pathJoin("netcdf", netcdf_ver))
+load(pathJoin("netcdf-D", netcdf_ver))
 load(pathJoin("bufr", bufr_ver))
 load(pathJoin("bacio", bacio_ver))
 load(pathJoin("w3emc", w3emc_ver))
-load(pathJoin("sp", sp_ver))
 load(pathJoin("ip", ip_ver))
 load(pathJoin("sigio", sigio_ver))
 load(pathJoin("sfcio", sfcio_ver))
