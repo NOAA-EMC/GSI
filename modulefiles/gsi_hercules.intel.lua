c36fec987c8146f5cf78ad24d59772990e698c1c
RussTreadon-NOAA <Russ.Treadon@noaa.gov>
2025-06-05 15:56:58 +0000

    update hera and hercules spack-stack/1.9.1 modulefiles (#887)

--- modulefiles/gsi_hercules.intel.lua
+++ modulefiles/gsi_hercules.intel.lua
@@ -2,19 +2,24 @@ help([[
 ]])
 
 prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-1.9.1/envs/ue-oneapi-2024.1.0/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-1.9.1/envs/ue-oneapi-2024.1.0/install/modulefiles/gcc/12.2.0")
 
-local stack_python_ver=os.getenv("stack_python_ver") or "3.11.6"
-local stack_intel_ver=os.getenv("stack_intel_ver") or "2024.2.1"
-local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
+local stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
+local stack_intel_oneapi_mpi_ver=os.getenv("stack_intel_oneapi_mpi_ver") or "2021.13"
+local intel_oneapi_mkl_ver=os.getenv("intel_oneapi_mkl_ver") or "2024.2.1"
+local stack_python_ver=os.getenv("stack_python_ver") or "3.11.7"
 local cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 
-load(pathJoin("stack-intel", stack_intel_ver))
-load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
-load(pathJoin("python", stack_python_ver))
+load(pathJoin("stack-oneapi", stack_oneapi_ver))
+load(pathJoin("stack-intel-oneapi-mpi", stack_intel_oneapi_mpi_ver))
+load(pathJoin("intel-oneapi-mkl", intel_oneapi_mkl_ver))
+load(pathJoin("stack-python", stack_python_ver))
 load(pathJoin("cmake", cmake_ver))
 
 load("gsi_common")
-load("intel-oneapi-mkl/2024.2.1")
+
+unload("intel-oneapi-mpi/2021.13.1")
+load("intel-oneapi-mpi/2021.7.1")
 
 pushenv("CFLAGS", "-xHOST")
 pushenv("FFLAGS", "-xHOST")
