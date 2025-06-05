eac7a3b978a0621369f993024dc52beca54ded0c
David Huber <david.huber@noaa.gov>
2025-06-05 12:43:06 +0000

    Update WCOSS2 module file versions

--- modulefiles/gsi_wcoss2.intel.lua
+++ modulefiles/gsi_wcoss2.intel.lua
@@ -12,10 +12,10 @@ local prod_util_ver=os.getenv("prod_util_ver") or "2.0.10"
 local netcdf_ver=os.getenv("netcdf_ver") or "4.9.2"
 local bufr_ver=os.getenv("bufr_ver") or "12.1.0"
 local bacio_ver=os.getenv("bacio_ver") or "2.4.1"
-local w3emc_ver=os.getenv("w3emc_ver") or "2.10.0"
-local ip_ver=os.getenv("ip_ver") or "5.1.0"
-local sigio_ver=os.getenv("sigio_ver") or "2.3.3"
-local sfcio_ver=os.getenv("sfcio_ver") or "1.4.2"
+local w3emc_ver=os.getenv("w3emc_ver") or "2.9.2"
+local ip_ver=os.getenv("ip_ver") or "5.0.0"
+local sigio_ver=os.getenv("sigio_ver") or "2.3.2"
+local sfcio_ver=os.getenv("sfcio_ver") or "1.4.1"
 local nemsio_ver=os.getenv("nemsio_ver") or "2.5.4"
 local wrf_io_ver=os.getenv("wrf_io_ver") or "1.2.0"
 local ncio_ver=os.getenv("ncio_ver") or "1.1.2"
@@ -35,14 +35,23 @@ load(pathJoin("netcdf-D", netcdf_ver))
 load(pathJoin("bufr", bufr_ver))
 load(pathJoin("bacio", bacio_ver))
 load(pathJoin("w3emc", w3emc_ver))
-load(pathJoin("ip", ip_ver))
+--load(pathJoin("ip", ip_ver))
+-- Temporarily define IP's paths here.
+-- TODO when testing is complete, request an official installation in https://github.com/NOAA-EMC/WCOSS2-requests/issues/11
+pushenv("ip_ROOT", "/apps/ops/para/libs/intel/19.1.3.304/ip/5.2.0")
+pushenv("IP_INC4", "/apps/ops/para/libs/intel/19.1.3.304/ip/5.2.0/include_4")
+pushenv("IP_INCd", "/apps/ops/para/libs/intel/19.1.3.304/ip/5.2.0/include_d")
+pushenv("IP_LIB4", "/apps/ops/para/libs/intel/19.1.3.304/ip/5.2.0/lib64/libip_4.a")
+pushenv("IP_LIBd", "/apps/ops/para/libs/intel/19.1.3.304/ip/5.2.0/lib64/libip_d.a")
+pushenv("ip_VERSION", "5.2.0")
+
 load(pathJoin("sigio", sigio_ver))
 load(pathJoin("sfcio", sfcio_ver))
 load(pathJoin("nemsio", nemsio_ver))
 load(pathJoin("wrf_io", wrf_io_ver))
 load(pathJoin("ncio", ncio_ver))
 load(pathJoin("crtm", crtm_ver))
-load(pathJoin("ncdiag",ncdiag_ver))
+load(pathJoin("ncdiag-A",ncdiag_ver))
 
 pushenv("GSI_BINARY_SOURCE_DIR", "/lfs/h2/emc/global/noscrub/emc.global/FIX/fix/gsi/20241022")
 
