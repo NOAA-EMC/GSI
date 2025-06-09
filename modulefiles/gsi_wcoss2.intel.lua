73678d9862109f94570a7f9ca095e2207e7afdc6
David Huber <david.huber@noaa.gov>
2025-06-09 13:28:59 +0000

    Revert netcdf to 4.7.4

--- modulefiles/gsi_wcoss2.intel.lua
+++ modulefiles/gsi_wcoss2.intel.lua
@@ -9,11 +9,7 @@ local cmake_ver= os.getenv("cmake_ver") or "3.20.2"
 local python_ver=os.getenv("python_ver") or "3.8.6"
 local prod_util_ver=os.getenv("prod_util_ver") or "2.0.10"
 
--- The netCDF-D library is dynamic and has bindings to HDF5 and pnetcdf.
-   -- Though these are not used by the GSI, they must be loaded to load netCDF-D
-local hdf5_ver=os.getenv("hdf5_ver") or "1.14.0"
-local pnetcdf_ver=os.getenv("pnetcdf_ver") or "1.12.2"
-local netcdf_ver=os.getenv("netcdf_ver") or "4.9.2"
+local netcdf_ver=os.getenv("netcdf_ver") or "4.7.4"
 
 local bufr_ver=os.getenv("bufr_ver") or "12.1.0"
 local bacio_ver=os.getenv("bacio_ver") or "2.4.1"
@@ -25,7 +21,7 @@ local nemsio_ver=os.getenv("nemsio_ver") or "2.5.4"
 local wrf_io_ver=os.getenv("wrf_io_ver") or "1.2.0"
 local ncio_ver=os.getenv("ncio_ver") or "1.1.2"
 local crtm_ver=os.getenv("crtm_ver") or "2.4.0.1"
-local ncdiag_ver=os.getenv("ncdiag_ver") or "1.1.2"
+local ncdiag_ver=os.getenv("ncdiag_ver") or "1.1.1"
 
 load(pathJoin("PrgEnv-intel", PrgEnv_intel_ver))
 load(pathJoin("intel", intel_ver))
@@ -36,9 +32,7 @@ load(pathJoin("python", python_ver))
 
 load(pathJoin("prod_util", prod_util_ver))
 
-load(pathJoin("hdf5-D", hdf5_ver))
-load(pathJoin("pnetcdf-D", pnetcdf_ver))
-load(pathJoin("netcdf-D", netcdf_ver))
+load(pathJoin("netcdf", netcdf_ver))
 
 load(pathJoin("bufr", bufr_ver))
 load(pathJoin("bacio", bacio_ver))
@@ -57,9 +51,9 @@ load(pathJoin("sigio", sigio_ver))
 load(pathJoin("sfcio", sfcio_ver))
 load(pathJoin("nemsio", nemsio_ver))
 load(pathJoin("wrf_io", wrf_io_ver))
-load(pathJoin("ncio-A", ncio_ver))
+load(pathJoin("ncio", ncio_ver))
 load(pathJoin("crtm", crtm_ver))
-load(pathJoin("ncdiag-A",ncdiag_ver))
+load(pathJoin("ncdiag",ncdiag_ver))
 
 pushenv("GSI_BINARY_SOURCE_DIR", "/lfs/h2/emc/global/noscrub/emc.global/FIX/fix/gsi/20241022")
 
