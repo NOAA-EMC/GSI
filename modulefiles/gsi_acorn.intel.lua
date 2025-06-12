9a9faad700599c3b932b6b411c80690dd59a354c
russ.treadon <Russ.Treadon@noaa.gov>
2025-06-12 12:59:20 +0000

    remove references to sp from gsi_acorn.intel.lua

--- modulefiles/gsi_acorn.intel.lua
+++ modulefiles/gsi_acorn.intel.lua
@@ -13,7 +13,6 @@ local netcdf_ver=os.getenv("netcdf_ver") or "4.7.4"
 local bufr_ver=os.getenv("bufr_ver") or "12.1.0"
 local bacio_ver=os.getenv("bacio_ver") or "2.4.1"
 local w3emc_ver=os.getenv("w3emc_ver") or "2.9.2"
-local sp_ver=os.getenv("sp_ver") or "2.3.3"
 local ip_ver=os.getenv("ip_ver") or "5.1.0"
 local sigio_ver=os.getenv("sigio_ver") or "2.3.2"
 local sfcio_ver=os.getenv("sfcio_ver") or "1.4.1"
@@ -36,7 +35,6 @@ load(pathJoin("netcdf", netcdf_ver))
 load(pathJoin("bufr", bufr_ver))
 load(pathJoin("bacio", bacio_ver))
 load(pathJoin("w3emc", w3emc_ver))
-load(pathJoin("sp", sp_ver))
 --load(pathJoin("ip", ip_ver))
 -- Temporarily define IP's paths here.
 -- TODO when testing is complete, request an official installation in https://github.com/NOAA-EMC/WCOSS2-requests/issues/11
