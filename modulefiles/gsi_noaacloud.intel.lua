d0fc2134694b2f938846f24306d1d7bdb59589d1
Taylor Roper <taylor.roper@noaa.gov>
2025-07-01 21:56:32 +0000

    Add gcc version and maintain gnu stack (#901)

--- modulefiles/gsi_noaacloud.intel.lua
+++ modulefiles/gsi_noaacloud.intel.lua
@@ -4,13 +4,13 @@ help([[
 prepend_path("MODULEPATH", "/contrib/spack-stack-rocky8/spack-stack-1.6.0/envs/gsi-addon-env/install/modulefiles/Core")
 prepend_path("MODULEPATH", "/apps/modules/modulefiles")
 
+local gcc_ver=os.getenv("gcc_ver") or "13.2.0"
 local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
 local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.10.0"
 
-load("gnu")
+load(pathJoin("gnu", gcc_ver))
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
-unload("gnu")
 
 load("gsi_common")
 
