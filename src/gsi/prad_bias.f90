59e15a1a082f08656675e8f333056c5f9fbb9a1d
David Huber <david.huber@noaa.gov>
2025-07-15 17:07:20 +0000

    Deallocate arrays before cycling the prod_bias loop

--- src/gsi/prad_bias.f90
+++ src/gsi/prad_bias.f90
@@ -310,8 +310,10 @@ contains
         jj=(mm-1)*npred+ii
         AA(i,i) = AA(i,i)+one/varprd(jj)
      end do
-     if (all(abs(AA)<atiny)) cycle
-     if (all(abs(be)<atiny)) cycle
+     if (all(abs(AA)<atiny) .or. all(abs(be)<atiny)) then
+        deallocate(AA,be)
+        cycle
+     endif
      call linmm(AA,be,kpred,1,kpred,kpred)
 
 
