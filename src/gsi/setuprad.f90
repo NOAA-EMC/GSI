bbf72ffbcbb46343a21f604dfc8c3b33d77049c8
Andrew Collard <andrew.collard@noaa.gov>
2025-09-03 14:27:10 +0000

    Add satid to error message

--- src/gsi/setuprad.f90
+++ src/gsi/setuprad.f90
@@ -1005,7 +1005,7 @@ contains
            write(*,*) 'WARNING: NaN found in CRTM simulated radiance output'
            do i = 1, nchanl
               if (ieee_is_nan(tsim(i))) then
-                  write(*,*) 'NaN for channel ', sc_index(i), ' at latitude = ', &
+                  write(*,*) 'NaN for ',trim(isis),' channel ', sc_index(i), ' at latitude = ', &
                           cenlat,' longitude = ',cenlon
               end if
            end do
