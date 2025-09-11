a8569207f483da99411bb902a2672c72af190689
David Huber <david.huber@noaa.gov>
2025-09-11 21:43:25 +0000

    Update craype and cray_mpich versions on WCOSS2; remove python

--- modulefiles/gsi_wcoss2.intel.lua
+++ modulefiles/gsi_wcoss2.intel.lua
@@ -3,10 +3,9 @@ help([[
 
 local PrgEnv_intel_ver=os.getenv("PrgEnv_intel_ver") or "8.1.0"
 local intel_ver=os.getenv("intel_ver") or "19.1.3.304"
-local craype_ver=os.getenv("craype_ver") or "2.7.10"
-local cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.7"
+local craype_ver=os.getenv("craype_ver") or "2.7.13"
+local cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.9"
 local cmake_ver= os.getenv("cmake_ver") or "3.20.2"
-local python_ver=os.getenv("python_ver") or "3.8.6"
 local prod_util_ver=os.getenv("prod_util_ver") or "2.0.10"
 
 local hdf5_ver=os.getenv("hdf5_ver") or "1.14.0"
@@ -30,7 +29,6 @@ load(pathJoin("intel", intel_ver))
 load(pathJoin("craype", craype_ver))
 load(pathJoin("cray-mpich", cray_mpich_ver))
 load(pathJoin("cmake", cmake_ver))
-load(pathJoin("python", python_ver))
 
 load(pathJoin("prod_util", prod_util_ver))
 
