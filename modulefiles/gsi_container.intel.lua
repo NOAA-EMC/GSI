ee260b28bea8695b2e9ad7ff5da6756992ed4988
Wei Huang <Wei.Huang@noaa.gov>
2025-09-15 17:29:11 +0000

    update to ss192 for container

--- modulefiles/gsi_container.intel.lua
+++ modulefiles/gsi_container.intel.lua
@@ -1,14 +1,14 @@
 help([[
 ]])
 
-prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.8.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.9.2/envs/unified-env/install/modulefiles/Core")
 
-local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
-local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.12.2"
+stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.0"
+stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
 local cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 local prod_util_ver=os.getenv("prod_util_ver") or "2.1.1"
 
-load(pathJoin("stack-intel", stack_intel_ver))
+load(pathJoin("stack-oneapi", stack_oneapi_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 load(pathJoin("cmake", cmake_ver))
 
