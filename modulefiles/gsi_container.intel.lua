0269f0311170ea386e6d13807a2dd1a29facdc51
Wei Huang <Wei.Huang@noaa.gov>
2025-09-15 22:07:41 +0000

    use ss192 in container

--- modulefiles/gsi_container.intel.lua
+++ modulefiles/gsi_container.intel.lua
@@ -3,26 +3,19 @@ help([[
 
 prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.9.2/envs/unified-env/install/modulefiles/Core")
 
-stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.0"
-stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
+local stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.0"
+local stack_intel_oneapi_mpi_ver=os.getenv("stack_intel_oneapi_mpi_ver") or "2021.13"
 local cmake_ver=os.getenv("cmake_ver") or "3.27.9"
-local prod_util_ver=os.getenv("prod_util_ver") or "2.1.1"
 
 load(pathJoin("stack-oneapi", stack_oneapi_ver))
-load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+load(pathJoin("stack-intel-oneapi-mpi", stack_intel_oneapi_mpi_ver))
 load(pathJoin("cmake", cmake_ver))
 
 load("gsi_common")
-load(pathJoin("prod_util", prod_util_ver))
 
-pushenv("CFLAGS", "-march=ivybridge")
-pushenv("FFLAGS", "-march=ivybridge")
+pushenv("CFLAGS", "-xHOST")
+pushenv("FFLAGS", "-xHOST")
 
-setenv("CC","mpiicc")
-setenv("CXX","mpiicpc")
-setenv("FC","mpiifort")
-setenv("F90","mpiifort")
-setenv("F77","mpiifort")
-pushenv("USE_BUFR4", "YES")
+pushenv("GSI_BINARY_SOURCE_DIR", "/contrib/global-workflow-shared-data/fix/gsi/20250529")
 
 whatis("Description: GSI environment in a container with Intel Compilers")
