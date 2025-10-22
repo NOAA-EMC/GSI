80ff4966165c800cbf660e0975b4317a622ea2a5
Taylor Roper <taylor.roper@noaa.gov>
2025-10-22 20:18:12 +0000

    Account and group not needed on cloud; cpus-per-task set in srun command

--- regression/regression_var.sh
+++ regression/regression_var.sh
@@ -178,13 +178,13 @@ case $machine in
   noaacloud)
 
     export noscrub="${noscrub:-/contrib/$USER/noscrub}"
-    #export group="${group:-$USER}"
+    export group="${group:-$USER}"
     export queue="${queue:-batch}"
     export ptmp="${ptmp:-/lustre/$USER/ptmp}"
     export casesdir="${casesdir:-/bucket/GSI_RTs}"
     export partition="${partition:-compute}"
     export check_resource="no"
-    #export accnt="${accnt:-ca-epic}"
+    export accnt="${accnt:-}"
     export clean=".false."
   ;;
 
