8e26e4a3e3b9080a3435c99579ceff4563c6ef6f
Taylor Roper <taylor.roper@noaa.gov>
2025-11-04 18:46:38 +0000

    Update noaacloud global RT procs/nodes/walltimes

--- regression/regression_param.sh
+++ regression/regression_param.sh
@@ -79,8 +79,8 @@ case $regtest in
            topts[1]="0:10:00" ; popts[1]="12/8/" ; ropts[1]="/1"
            topts[2]="0:10:00" ; popts[2]="12/10/" ; ropts[2]="/2"
 	elif [[ "$machine" = "noaacloud" ]]; then
-           topts[1]="0:20:00" ; popts[1]="48/2/" ; ropts[1]="/1"
-           topts[2]="0:20:00" ; popts[2]="40/3/" ; ropts[2]="/1"
+           topts[1]="0:10:00" ; popts[1]="36/1/" ; ropts[1]="/1"
+           topts[2]="0:10:00" ; popts[2]="24/2/" ; ropts[2]="/2"
         fi
 
         if [ "$debug" = ".true." ] ; then
@@ -262,8 +262,8 @@ case $regtest in
            topts[1]="0:10:00" ; popts[1]="16/2/" ; ropts[1]="/1"
            topts[2]="0:10:00" ; popts[2]="16/4/" ; ropts[2]="/2"
 	elif [[ "$machine" = "noaacloud" ]]; then
-	   topts[1]="0:20:00" ; popts[1]="36/1/" ; ropts[1]="/1"
-	   topts[2]="0:20:00" ; popts[2]="30/2/" ; ropts[2]="/1"
+	   topts[1]="0:10:00" ; popts[1]="36/1/" ; ropts[1]="/1"
+	   topts[2]="0:10:00" ; popts[2]="48/1/" ; ropts[2]="/1"
         fi
 
         if [ "$debug" = ".true." ] ; then
