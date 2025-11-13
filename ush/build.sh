092c138b32904701e5a002fab9483f46349a8d51
Gang Zhao <gang.zhao@noaa.gov>
2025-11-13 21:10:46 +0000

    Following reviewers' suggestions (on PR #948), using the option arguments   to pass the building options into ush/build.sh,   instead of using positional arguments.  (for issue #946 and PR #948)

--- ush/build.sh
+++ ush/build.sh
@@ -2,22 +2,32 @@
 
 set -eux
 
-# checking if the first positional argument is specified to build a special system
-if [[ $# -ge 1 ]] ; then
-    DASYS_NAME="$1"
-    if [[ ${DASYS_NAME,,} == "3drtma" ||  ${DASYS_NAME,,} == "rtma3d" ]] ; then
-        BUILD_GSI4RTMA3D="Yes"
-        echo " ****** Building GSI for 3D-RTMA ****** "
-    else
-        unset BUILD_GSI4RTMA3D
-    fi
-fi
-unset DASYS_NAME
+#=================================================================================================#
+usage() {
+#   This usage function is based on the usage function in the build.sh in NOAA-EMC RDASApp 
+#   for JEDI-based DA pacakge. see
+#   https://github.com/NOAA-EMC/RDASApp/blob/e9ea4c1c02ece82931bf66d2f36b6009a921f6d9/build.sh
+  set +x
+  echo
+  echo "Usage options: "
+  echo "  -c additional CMake options				DEFAULT: <none>; (GSDCLOUD: using GSD Cloud Analysis)"
+  echo "  -v build with verbose output				DEFAULT: do not use this flag"
+  echo "  -l save the building log files under build directory	DEFAULT: do not use this flag"
+  echo "  -h display this usage/help information and quit"
+  echo "Usage examples: "
+  echo "  $0                    # building GSI with default config (for most DA systems, e.g. GDAS, RRFS-DA, etc.)"
+  echo "  $0 -c GSDCLOUD        # building GSI with GSD Cloud Analysis support (only for RAP/HRRR, 3DRTMA, not default)"
+  echo "  $0 -v                 # building GSI with verbose output (not default)"
+  echo "  $0 -l                 # saving the cmake/make/install log into log files under build directory (not default)"
+  echo 
+  exit 1
+}
+#=================================================================================================#
 
 # Get the root of the cloned GSI directory
 readonly DIR_ROOT=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )/.." && pwd -P)
 
-# User Options
+# User Options -- Defaults
 BUILD_TYPE=${BUILD_TYPE:-"Release"}
 CMAKE_OPTS=${CMAKE_OPTS:-}
 COMPILER=${COMPILER:-"intel"}
@@ -31,6 +41,44 @@ REGRESSION_TESTS=${REGRESSION_TESTS:-"YES"} # Build regression test suite
 
 # Detect machine (sets MACHINE_ID)
 source $DIR_ROOT/ush/detect_machine.sh
+
+# checking if any specific option(s) is specified through the option arguments
+unset OPTS4CMAKE
+unset SAVELOG
+unset BUILD_GSDCLDANL
+while getopts "c:hlv" opt; do
+  case ${opt} in
+    c)
+      OPTS4CMAKE=${OPTARG}
+      ;;
+    v)
+      BUILD_VERBOSE=1
+      ;;
+    l)
+      SAVELOG="Yes"
+      ;;
+    h|\?|\:)
+      usage
+      ;;
+  esac
+done
+
+shift $((OPTIND - 1)) # Shift positional parameters to remove parsed options
+
+shopt -s nocasematch		# enable case-insensitive matching for patterns
+if [[ -v OPTS4CMAKE ]] ; then
+  case "${OPTS4CMAKE}" in
+    gsdcloud|gsdcldanl|gsdcld|gsdcloudanalysis|cloudanalysis|cldanl)
+      BUILD_GSDCLDANL="Yes"
+      echo " ****** Building GSI with GSD Cloud Analysis Support ****** "
+      ;;
+    *)
+      unset BUILD_GSDCLDANL
+      ;;
+  esac
+fi
+shopt -u nocasematch		# disable case-insensitive matching for patterns
+unset OPTS4CMAKE
 
 # Load modules
 set +x
@@ -52,9 +100,9 @@ CMAKE_OPTS+=" -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX"
 CMAKE_OPTS+=" -DGSI_MODE=$GSI_MODE -DENKF_MODE=${ENKF_MODE}"
 
 # Configure for building GSI with GSD Cloud Analysis
-if [[ -v BUILD_GSI4RTMA3D && ${BUILD_GSI4RTMA3D} =~ [yYtT] ]] ; then
-    BUILD_GSDCLOUD=${BUILD_GSDCLOUD:-"ON"}      # Build GSD Cloud Analysis library
-    USE_GSDCLOUD=${USE_GSDCLOUD:-"ON"}          # Build with GSD Cloud Analysis library
+if [[ -v BUILD_GSDCLDANL && ${BUILD_GSDCLDANL} =~ [yYtT] ]] ; then
+    BUILD_GSDCLOUD="ON"				# Build GSD Cloud Analysis library
+    USE_GSDCLOUD="ON"				# Build with GSD Cloud Analysis library
     CMAKE_OPTS+=" -DBUILD_GSDCLOUD=${BUILD_GSDCLOUD} -DUSE_GSDCLOUD=${USE_GSDCLOUD}"
 fi
 
@@ -67,10 +115,9 @@ mkdir -p $BUILD_DIR && cd $BUILD_DIR
 
 # Configure, build, install
 #     specifit options for 3DRTMA
-if [[ -v BUILD_GSI4RTMA3D && ${BUILD_GSI4RTMA3D} =~ [yYtT] ]] ; then
-    echo " ****** Building GSI with GSD Cloud Analysis for 3D-RTMA ****** "
+if [[ -v SAVELOG && ${SAVELOG} =~ [yYtT] ]] ; then
     cmake $CMAKE_OPTS $DIR_ROOT 2>&1 | tee log.cmake
-    make -j ${BUILD_JOBS:-8} VERBOSE=${BUILD_VERBOSE:-1} 2>&1 | tee log.make
+    make -j ${BUILD_JOBS:-8} VERBOSE=${BUILD_VERBOSE:-} 2>&1 | tee log.make
     make install 2>&1 | tee log.install
 else
     cmake $CMAKE_OPTS $DIR_ROOT
