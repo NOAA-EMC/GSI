a1007cb64379ecba804eb52688bd8d85ce095df8
Gang Zhao <gang.zhao@noaa.gov>
2025-12-01 22:58:58 +0000

    Change variable name "i_gsd_terrain_match_mso" to "i_gsd_terrain_match_mesonet" (Issue #954)

--- src/gsi/rapidrefresh_cldsurf_mod.f90
+++ src/gsi/rapidrefresh_cldsurf_mod.f90
@@ -251,7 +251,7 @@ module rapidrefresh_cldsurf_mod
 !                          = 0 (vis-off: default) : no analysis of visibility in 3D analysis.
 !                          = 1 (vis-on) : if variable name "vis" is found in anavinfo,
 !                                          set it to be 1 to turn on analysis of visibility;
-!      i_gsd_terrain_match_mso - namelist integer, control application of GSD Terrain Match to MESONET (MSO)
+!      i_gsd_terrain_match_mesonet - namelist integer, control application of GSD Terrain Match to MESONET (MSO)
 !                                observations of Temp (188, 195)
 !                          = 0 : do not apply GSD terrain match to MESONET Obs of T (default)
 !                          = 1 : apply GSD terrain match to MESONET Obs of T
@@ -334,7 +334,7 @@ module rapidrefresh_cldsurf_mod
   public :: i_sfcrough_fgs
   public :: corp_vis, hwllp_vis
   public :: i_vis_3dda
-  public :: i_gsd_terrain_match_mso
+  public :: i_gsd_terrain_match_mesonet
 
   logical l_hydrometeor_bkio
   real(r_kind)  dfi_radar_latent_heat_time_period
@@ -401,7 +401,7 @@ module rapidrefresh_cldsurf_mod
   integer(i_kind)   :: i_sfcrough_fgs
   real(r_kind)      :: corp_vis, hwllp_vis
   integer(i_kind)   :: i_vis_3dda
-  integer(i_kind)   :: i_gsd_terrain_match_mso
+  integer(i_kind)   :: i_gsd_terrain_match_mesonet
 
 contains
 
@@ -551,7 +551,7 @@ contains
 
     i_vis_3dda          = 0                           ! no analysis of visibility (vis) in 3D analysis (default)
 
-    i_gsd_terrain_match_mso = 0                       ! no GSD terrain match applied to METSONET obs (default)
+    i_gsd_terrain_match_mesonet = 0                   ! no GSD terrain match applied to METSONET obs (default)
 
 !-- searching for specific variable in state variable list (reading from anavinfo)
     do i2=1,ns2d
