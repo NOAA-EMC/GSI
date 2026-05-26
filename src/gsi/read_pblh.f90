dc2bd594e0c307f7955dcb5a66e3e77f4bae106c
Jiarui Dong <Jiarui.Dong@noaa.gov>
2026-05-26 09:45:44 -0400

    Refactor write statements for nlevp output

--- src/gsi/read_pblh.f90
+++ src/gsi/read_pblh.f90
@@ -436,8 +436,8 @@
       write(*,'(1x,f7.2,2(1x,f6.2),$)') (hdr(i),i=2,4)           ! XOB,YOB,DHR
       write(*,'(1x,i6,1x,i3,1x,i4,1x,i3,$)') (int(hdr(i)),i=5,8) ! ELV,TYP,T29,ITP
 
-      write(*,'(1x,2(2x,a,1x,i3),a,$)') '(olv=',nlevo,'nlevp=',nlevp,')'
-      write(*,'(1x,2(2x,a,1x,i3),a,$)') '(clv=',nlevc,'nlevp=',nlevp,')'
+      write(*,'("(nlevp=",i3,") ")', advance='no') nlevp
+      write(*,'("(nlevp=",i3,")")') nlevp
 
       ndata=ndata+1
       nodata=nodata+1
