f41b5c73e80cd957e34f975d57fda506c873cc62
RussTreadon-NOAA <Russ.Treadon@noaa.gov>
2026-08-05 11:14:36 +0000

    update ursa modulefile to crtm-fix/3.1.2.0, remove hardwired CRTM_FIX

--- modulefiles/gsi_ursa.intel.lua
+++ modulefiles/gsi_ursa.intel.lua
@@ -7,7 +7,7 @@ local stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2025.3.1"
 local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.17"
 local oneapi_mkl_ver=os.getenv("oneapi_mkl_ver") or "2025.3"
 local cmake_ver=os.getenv("cmake_ver") or "3.31.8"
-local crtm_fix_ver=os.getenv("crtm_fix_ver") or "3.1.2"
+local crtm_fix_ver=os.getenv("crtm_fix_ver") or "3.1.2.0"
 
 load(pathJoin("stack-intel-oneapi-compilers", stack_oneapi_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
@@ -17,6 +17,5 @@ load(pathJoin("cmake", cmake_ver))
 load("gsi_common")
 
 pushenv("GSI_BINARY_SOURCE_DIR", "/scratch3/NCEPDEV/global/role.glopara/fix/gsi/20251105")
-setenv("CRTM_FIX", "/scratch3/NCEPDEV/da/Andrew.Collard/git/CRTMv3_GSI/fix_LittleEndian")
 
 whatis("Description: GSI environment on Ursa with Intel Compilers")
